a55ae0ae87bb3d5049559653ef8a08fa8b5d5af4
Jere Helén <jere.helen@helsinki.fi>
2020-02-11 13:47:42 +0200

    Add filtering to analytic view

--- client/components/AnalyticsPage/index.js
+++ client/components/AnalyticsPage/index.js
@@ -1,20 +1,50 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import Dropdown from 'Components/Generic/Dropdown'
 import { useDispatch, useSelector } from 'react-redux'
 import { getAnswersAction } from 'Utilities/redux/answersReducer'
-import { allLightIds, programmes } from 'Utilities/common'
+import { degreeLevels, allLightIds, programmes } from 'Utilities/common'
 
 export default () => {
   const dispatch = useDispatch()
   const answers = useSelector((state) => state.answers)
 
+  const [chosenDegreeLevel, setDegree] = useState('')
+
+  const handleChange = async ({ target }) => {
+    const { value } = target
+    setDegree(value)
+  }
+
   useEffect(() => {
     dispatch(getAnswersAction())
   }, [])
+
+  /**
+   * Copy of total insanity
+   */
+
+  const filteredProgrammes = programmes.filter((prog) => {
+    if (!chosenDegreeLevel || chosenDegreeLevel === 'All levels') return true
+
+    const starting = prog.substr(0, 4)
+    const shortDegrees = degreeLevels.map((dl) => dl.substr(0, 4))
+    if (!shortDegrees.includes(starting)) return true
+
+    if (chosenDegreeLevel.includes(starting)) return true
+    return false
+  })
 
   if (answers.pending || !answers.data) return <>SPINNING!</>
 
   return (
     <>
+      <Dropdown
+        id="degree_level"
+        value={chosenDegreeLevel}
+        label="Degree level"
+        options={['All levels'].concat(degreeLevels)}
+        onChange={handleChange}
+      />
       <table className="ui fixed celled striped table">
         <thead>
           <tr>
@@ -25,7 +55,7 @@ export default () => {
           </tr>
         </thead>
         <tbody>
-          {programmes.map((p) => {
+          {filteredProgrammes.map((p) => {
             const programme = answers.data.find((a) => a.programme === p)
             if (!programme)
               return (
