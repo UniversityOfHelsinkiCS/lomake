78e77e830149d871ee9f42c7a76b6f753ab532bd
Jere Helén <jere.helen@helsinki.fi>
2020-02-25 15:05:08 +0200

    Add tokens table

---
+++ server/models/token.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, DataTypes) => {
+  const token = sequelize.define(
+    'token',
+    {
+      url: DataTypes.STRING,
+      programme: DataTypes.STRING,
+      type: DataTypes.ENUM('ADMIN', 'WRITE', 'READ'),
+      usageCounter: {
+        type: DataTypes.INTEGER,
+        field: 'usage_counter'
+      }
+    },
+    {
+      underscored: true,
+      tableName: 'tokens'
+    }
+  )
+
+  return token
+}
