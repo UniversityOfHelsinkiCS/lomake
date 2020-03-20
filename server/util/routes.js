fd2ee29d4ede094a6a51ae55ec1c1f8cf79a9783
Sendou <sendou77@gmail.com>
2020-03-20 13:55:46 +0200

    deleted unused formcontroller

--- server/util/routes.js
+++ server/util/routes.js
@@ -1,16 +1,10 @@
 const Router = require('express')
-const forms = require('@controllers/formsController')
 const users = require('@controllers/usersController')
 const answers = require('@controllers/answersController')
 const tokens = require('@controllers/tokensController')
 const { checkAdmin } = require('@middleware/accessControlMiddleware')
 
 const router = Router()
-
-router.get('/', (req, res) => res.send('welcome to root'))
-
-router.get('/form', forms.getOne)
-router.put('/form', forms.createOrUpdate)
 
 router.get('/answers', checkAdmin, answers.getAll)
 router.get('/answers/temp', checkAdmin, answers.getAllTemp)
