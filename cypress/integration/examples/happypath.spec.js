2ec81bbab858d54407cb80f669b8568159eaa25f
Koskinen Marko J <markokoskinen2037@gmail.com>
2020-04-06 20:18:12 +0300

    made a test to type something in the textareas

--- cypress/integration/examples/happypath.spec.js
+++ cypress/integration/examples/happypath.spec.js
@@ -22,8 +22,18 @@ describe('Form tests', function () {
     cy.visit("http://localhost:8000/form/Bachelor's%20Programme%20in%20Computer%20Science")
   })
 
-  it('Can open a question and click on a face', function () {
-    cy.get('[data-cy=form-section-I]').click() // Opens question 1
-    cy.get('[data-cy=form-section-I]').get('[data-cy=street-light-neutral]').click()
+  it('Can open a question, select a face and type in the textfield', function () {
+    cy.get('[data-cy=form-section-I]').click()
+    cy.get('[data-cy=street-light-neutral-review_of_last_years_situation_report]').click()
+    cy.get('[data-cy=textarea-review_of_last_years_situation_report]')
+      .find('.editor-class')
+      .type('Yes. Last year went very well.')
+
+    cy.get('[data-cy=form-section-III]').click()
+    cy.get('[data-cy=textarea-wellbeing_information_used]')
+      .find('.editor-class')
+      .type('Google was a good source.')
+
+    cy.get('[data-cy=street-light-positive-community_wellbeing]').click()
   })
 })
