9f50da04a2dc3b49649d9d767aa85f485a283279
Marko Koskinen <markokoskinen2037@gmail.com>
2020-04-08 18:23:34 +0300

    added tests for the checkmarks

--- cypress/integration/examples/happypath.spec.js
+++ cypress/integration/examples/happypath.spec.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-undef */
 /// <reference types="cypress" />
 
+import * as _ from "lodash"
+
 function makeid(length) {
   var result = ''
   var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
@@ -30,6 +32,15 @@ describe('Core tests', function () {
 describe('Form tests', function () {
   this.beforeEach(function () {
     cy.visit("http://localhost:8000/form/Bachelor's%20Programme%20in%20Computer%20Science")
+  })
+
+  // This function just clears the forms' input fields
+  this.beforeAll(function () {
+    cy.get("[data-cy^=form-section").click({ multiple: true })
+    cy.get(".editor-class").each(function (el, index, list) {
+      cy.get(el).click()
+      cy.focused().clear()
+    })
   })
 
   it('Can open a question, click on smily face, and the result it saved.', () => {
@@ -69,4 +80,33 @@ describe('Form tests', function () {
       .find('.editor-class')
       .should('contain.text', testString2)
   })
+
+  it("Can click next and see a checkmark if answer is valid", function () {
+    cy.get('[data-cy=form-section-I]').click()
+    cy.get('[data-cy=textarea-review_of_last_years_situation_report]').find(".editor-class").click()
+    cy.focused().clear()
+    cy.focused().type("This text is long enough, but not too long. Therefore I want a green checkmark.")
+    cy.get('[data-cy=form-section-I-nextbutton]').click()
+
+    cy.get('[data-cy=form-section-I]').find(".check")
+  })
+
+  it("Can click next and see an error mark if required answers are missing", function () {
+    cy.get('[data-cy=form-section-II]').click()
+    cy.get('[data-cy=form-section-II-nextbutton]').click()
+    cy.get('[data-cy=form-section-II]').find(".close")
+  })
+
+
+  it("Cant click next and error is shown if text is too long", function () {
+    cy.get('[data-cy=form-section-II]').click()
+    cy.get('[data-cy=textarea-overall_status_information_used]').find(".editor-class").click()
+    cy.focused().clear()
+    cy.focused().type("aaaaaaaaaa".repeat(101), { delay: 0 })
+
+    cy.get('[data-cy=form-section-II-nextbutton]').should("be.disabled")
+    cy.contains("One or more answers that are too long")
+
+  })
+
 })
