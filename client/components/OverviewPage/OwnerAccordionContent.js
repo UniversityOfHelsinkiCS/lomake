e809fb8ce305a4889292573e38d42b5e682289dc
Marko Koskinen <markokoskinen2037@gmail.com>
2020-04-15 21:01:12 +0300

    refactoring + lint

--- client/components/OverviewPage/OwnerAccordionContent.js
+++ client/components/OverviewPage/OwnerAccordionContent.js
@@ -1,24 +1,21 @@
 import React, { useEffect, useState } from 'react'
-import { useSelector, useDispatch } from "react-redux"
+import { useSelector, useDispatch } from 'react-redux'
 import OwnerAccordionLinks from './OwnerAccordionLinks'
 import OwnerAccordionUsers from './OwnerAccordionUsers'
 import { getProgrammesUsersAction } from 'Utilities/redux/programmesUsersReducer'
 import { getProgrammesTokensAction } from 'Utilities/redux/programmesTokensReducer'
 
-const OwnerAccordionContent = ({ program }) => {
-
-
+const OwnerAccordionContent = ({ programKey }) => {
   const dispatch = useDispatch()
   const [dataLoading, setDataLoading] = useState(false)
   const [dataReady, setDataReady] = useState(false)
   const usersPending = useSelector(({ programmesUsers }) => programmesUsers.pending)
   const tokensPending = useSelector(({ programmesTokens }) => programmesTokens.pending)
 
-
   useEffect(() => {
     setDataLoading(true)
-    dispatch(getProgrammesUsersAction(program))
-    dispatch(getProgrammesTokensAction(program))
+    dispatch(getProgrammesUsersAction(programKey))
+    dispatch(getProgrammesTokensAction(programKey))
   }, [])
 
   useEffect(() => {
@@ -28,14 +25,12 @@ const OwnerAccordionContent = ({ program }) => {
     }
   }, [usersPending, tokensPending])
 
-
   if (!dataReady) return null
-
 
   return (
     <>
-      <OwnerAccordionLinks programme={program} />
-      <OwnerAccordionUsers programme={program} />
+      <OwnerAccordionLinks programme={programKey} />
+      <OwnerAccordionUsers programme={programKey} />
     </>
   )
 }
