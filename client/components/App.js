632442399d1a5d8d8dc81125ca6a23d23c15cad5
Marko Koskinen <markokoskinen2037@gmail.com>
2020-04-15 21:01:46 +0300

    before rendering anything, make sure studyprogrammes are ready

--- client/components/App.js
+++ client/components/App.js
@@ -5,17 +5,20 @@ import { wsConnect } from 'Utilities/redux/websocketReducer'
 import { loginAction } from 'Utilities/redux/currentUserReducer'
 import NavBar from 'Components/NavBar'
 import Router from 'Components/Router'
+import { getStudyProgrammes } from 'Utilities/redux/studyProgrammesReducer'
 
 export default () => {
   const dispatch = useDispatch()
   const currentUser = useSelector((state) => state.currentUser)
+  const studyProgrammes = useSelector(({ studyProgrammes }) => studyProgrammes.data)
 
   useEffect(() => {
     dispatch(loginAction())
     dispatch(wsConnect())
+    dispatch(getStudyProgrammes())
   }, [])
 
-  if (!currentUser.data) return null
+  if (!currentUser.data || !studyProgrammes) return null
 
   return (
     <div>
