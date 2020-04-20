d560353f6374091e0c646eeee1bc757dca16a46f
Marko Koskinen <markokoskinen2037@gmail.com>
2020-04-20 19:45:44 +0300

    give cypress user different kinds of permissions

--- server/scripts/createCypressUser.js
+++ server/scripts/createCypressUser.js
@@ -4,23 +4,28 @@ const logger = require('@util/logger')
 const createCypressUser = async () => {
   try {
     logger.info('Creating cypressUser for tests')
-    await db.user.findOrCreate({
-      where: {
-        uid: 'cypressUser',
-      },
-      defaults: {
-        name: 'cypressUser',
-        email: 'cypressUser',
-        admin: false,
-        access: {
-          bsc_computer_science: {
-            read: true,
-            admin: true,
-            write: true,
-          },
+
+    await db.user.destroy({ where: { uid: 'cypressUser' } }) //Delete old one if exists
+
+    await db.user.create({
+      uid: 'cypressUser',
+      name: 'cypressUser',
+      email: 'cypressUser',
+      admin: false,
+      access: {
+        bsc_computer_science: {
+          read: true,
+          admin: true,
+          write: true,
         },
-        irrelevant: false,
+        bsc_teachers_of_mathematics_physics_and_chemistry: {
+          read: true,
+        },
+        bsc_agricultural_sciences: {
+          write: true,
+        },
       },
+      irrelevant: false,
     })
   } catch (error) {
     logger.error(`Failed to create cypressUser: ${error}`)
