c192ad4ee0bbe9b3902e77cf0314d0477d1f5b5f
Marko Koskinen <markokoskinen2037@gmail.com>
2020-04-20 20:46:24 +0300

    added tests for permissions

--- cypress/integration/happypath.spec.js
+++ cypress/integration/happypath.spec.js
@@ -124,3 +124,36 @@ describe('Form tests', function () {
     cy.get('[data-cy=form-section-II]').find('.close')
   })
 })
+
+describe.only('Permission tests', function () {
+  it('Invalid url shows error', function () {
+    cy.visit('http://localhost:8000/form/lmao')
+    cy.contains('Error: Invalid url.')
+  })
+
+  it("Can't access form without permissions", function () {
+    cy.visit('http://localhost:8000/form/bsc_languages')
+    cy.contains('Error: No permissions.')
+  })
+
+  it("Can't WRITE with READ permissions and cant go to edit mode", function () {
+    cy.visit('http://localhost:8000/form/bsc_teachers_of_mathematics_physics_and_chemistry')
+
+    //Check that cant edit stuff:
+    cy.get('[data-cy=form-section-I]').click() // Simulate open attept even though does not do anything
+    cy.get('.editor-class').should('not.exist')
+    cy.get('[data-cy=pdfdownload-go-back-button]').should('not.exist')
+  })
+
+  it('Can WRITE form with WRITE permissions and switch to readmode', function () {
+    cy.visit('http://localhost:8000/form/bsc_agricultural_sciences')
+    cy.get('[data-cy=form-section-I]').click()
+    cy.get('[data-cy=textarea-review_of_last_years_situation_report]').find('.editor-class').click()
+    cy.focused().type('EDITING')
+    cy.get('[data-cy=pdfdownload-go-to-readmode]').click()
+  })
+
+  it('Can do management with ADMIN permissions', function () {
+    cy.visit('http://localhost:8000')
+  })
+})
