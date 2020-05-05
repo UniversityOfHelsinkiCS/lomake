65adb4f4a3f22899c8f5b4f740e026b07d58e350
Marko Koskinen <markokoskinen2037@gmail.com>
2020-05-05 15:30:03 +0300

    if hijacking show hijacker in logs

--- server/middleware/requestLoggerMiddleware.js
+++ server/middleware/requestLoggerMiddleware.js
@@ -1,10 +1,15 @@
 const logger = require('@util/logger')
 
 const requestLoggerMiddleware = (req, res, next) => {
+  const loggedInAs = req.headers['x-admin-logged-in-as']
+  const userString = loggedInAs
+    ? `User: ${req.headers.uid} mocking as ${loggedInAs}`
+    : `User: ${req.headers.uid}`
+
   logger.info(`Method: ${req.method}`)
   logger.info(`Path: ${req.path}`)
   logger.info(`Body: ${JSON.stringify(req.body)}`)
-  logger.info(`User: ${req.headers.uid}`)
+  logger.info(userString)
   logger.info('---')
 
   next()
