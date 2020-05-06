70dcd49d8c683b9558c4d4f7b18f6c3c1d72e33e
Marko Koskinen <markokoskinen2037@gmail.com>
2020-05-06 21:38:18 +0300

    added some token link tests

--- cypress/integration/mics.spec.js
+++ cypress/integration/mics.spec.js
@@ -3,13 +3,36 @@
 
 describe('Misc tests', function () {
   this.beforeEach(function () {
-    cy.visit('http://localhost:8000/form/KH50_005')
+    cy.visit('http://localhost:8000')
   })
 
   it('Locale can be changed and translations work', function () {
+    cy.visit('http://localhost:8000/form/KH50_005')
     cy.contains('Answers are saved automatically. ')
     cy.get('[data-cy=navBar-localeDropdown]').click()
     cy.get('[data-cy=navBar-localeOption-fi]').click()
     cy.contains('Vastaukset tallentuvat automaattisesti.')
+  })
+
+  it('Access keys are pre-generated', function () {
+    cy.get('[data-cy=KH80_001-manage]').click()
+    cy.get('[data-cy=KH80_001-viewlink] > input').invoke('val').should('contain', '/access/')
+    cy.get('[data-cy=KH80_001-editlink] > input').invoke('val').should('contain', '/access/')
+  })
+
+  it('Access link can be reset/updated', function () {
+    cy.get('[data-cy=KH80_001-manage]').click()
+
+    cy.get('[data-cy=KH80_001-viewlink] > input')
+      .invoke('val')
+      .then((text) => {
+        const initialLink = text
+
+        cy.get('[data-cy=KH80_001-viewlink-reset]').click()
+
+        cy.get('[data-cy=KH80_001-viewlink] > input')
+          .invoke('val')
+          .then((text) => expect(text).not.equal(initialLink))
+      })
   })
 })
