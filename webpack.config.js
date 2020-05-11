4349e67e5a13ab55e2a1ab366a0bbf4cf4d6ef77
Marko Koskinen <markokoskinen2037@gmail.com>
2020-05-11 18:37:41 +0300

    change default title to "Form"

--- webpack.config.js
+++ webpack.config.js
@@ -95,7 +95,7 @@ module.exports = (env, argv) => {
       }),
       // Skip the part where we would make a html template
       new HtmlWebpackPlugin({
-        title: 'Lomake',
+        title: 'Form',
         favicon: path.resolve(__dirname, 'client/assets/favicon-32x32.png'),
         inject: false,
         template: htmlTemplate,
