909cb6c037eba6baccb235ad0ac69e596978f005
Sendou <38327916+Sendouc@users.noreply.github.com>
2020-05-11 15:42:42 +0300

    customModal for questions

--- client/components/Generic/CustomModal.js
+++ client/components/Generic/CustomModal.js
@@ -3,14 +3,19 @@ import './CustomModal.scss'
 import { Icon } from 'semantic-ui-react'
 import useOnClickOutside from '../../util/useOnClickOutside'
 
-const CustomModal = ({ children, title, closeModal }) => {
+const CustomModal = ({ children, title, closeModal, borderColor }) => {
   const ref = useRef()
   useOnClickOutside(ref, closeModal)
+  console.log('borderColor', borderColor)
   return (
     <div className="outer">
-      <div className="inner" ref={ref}>
+      <div
+        className="inner"
+        style={borderColor ? { border: '3px solid', borderColor } : {}}
+        ref={ref}
+      >
         <div className="title-and-close">
-          {title}
+          <span style={{ maxWidth: '75%' }}>{title}</span>
           <Icon name="close" onClick={closeModal} style={{ cursor: 'pointer' }} />
         </div>
         {children}
