32bf609926d5f3815f2cc597aa7914eb972cd476
Marko Koskinen <markokoskinen2037@gmail.com>
2020-05-14 16:55:31 +0300

    refactor cypressUser generation

--- server/scripts/createCypressUsers.js
+++ server/scripts/createCypressUsers.js
@@ -1,44 +1,15 @@
 const db = require('@models/index')
 const logger = require('@util/logger')
+const { cypressUsers } = require('@util/common')
 
 const createCypressUsers = async () => {
   try {
     logger.info('Creating users for Cypress tests')
 
-    await db.user.destroy({ where: { uid: 'cypressUser' } })
-    await db.user.destroy({ where: { uid: 'cypressAdminUser' } })
-
-    await db.user.create({
-      uid: 'cypressUser',
-      firstname: 'cyp',
-      lastname: 'res',
-      email: 'cypressUser',
-      admin: false,
-      access: {
-        KH80_001: {
-          read: true,
-          admin: true,
-          write: true,
-        },
-        KH50_004: {
-          read: true,
-        },
-        KH50_005: {
-          write: true,
-          read: true,
-        },
-      },
-      irrelevant: false,
-    })
-
-    await db.user.create({
-      uid: 'cypressAdminUser',
-      firstname: 'cypress',
-      lastname: 'admin',
-      email: 'cypressAdminUser',
-      admin: true,
-      irrelevant: false,
-    })
+    for (const user of cypressUsers) {
+      await db.user.destroy({ where: { uid: user.uid } })
+      await db.user.create(user)
+    }
   } catch (error) {
     logger.error(`Failed to create users for Cypress tests: ${error}`)
   }
--- server/util/common.js
+++ server/util/common.js
@@ -1149,9 +1149,56 @@ const programmes = [
   },
 ]
 
+const cypressUsers = [
+  {
+    uid: 'cypressUser',
+    firstname: 'cyp',
+    lastname: 'res',
+    email: 'cypressUser',
+    admin: false,
+    access: {
+      KH80_001: {
+        read: true,
+        admin: true,
+        write: true,
+      },
+      KH50_004: {
+        read: true,
+      },
+      KH50_005: {
+        write: true,
+        read: true,
+      },
+    },
+    irrelevant: false,
+  },
+  {
+    uid: 'cypressUser2',
+    firstname: 'cyp2',
+    lastname: 'res2',
+    email: 'cypressUser2',
+    admin: false,
+    access: {
+      KH80_001: {
+        read: true,
+      },
+    },
+    irrelevant: false,
+  },
+  {
+    uid: 'cypressAdminUser',
+    firstname: 'cypress',
+    lastname: 'admin',
+    email: 'cypressAdminUser',
+    admin: true,
+    irrelevant: false,
+  },
+]
+
 module.exports = {
   ...common,
   DB_URL,
   PORT,
   programmes,
+  cypressUsers,
 }
