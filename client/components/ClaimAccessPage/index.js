a5567f29474eb93c8647b889e5c2190574c16024
Marko Koskinen <markokoskinen2037@gmail.com>
2020-05-18 16:37:56 +0300

    show list of programmes + refactor

--- client/components/ClaimAccessPage/index.js
+++ client/components/ClaimAccessPage/index.js
@@ -1,18 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getTokenAction, claimTokenAction } from 'Utilities/redux/accessTokenReducer'
-import { Button, Message, Icon, Input, Loader } from 'semantic-ui-react'
+import { Button, Message, Icon, Input, Loader, List } from 'semantic-ui-react'
 
 const translations = {
   prompt: {
     fi: 'Olet vastaanottamassa oikeuksia',
     en: 'You are claiming permissions',
     se: 'Du håller på att ta emot åtkomst',
-  },
-  promptFaculty: {
-    fi: 'Olet vastaanottamassa lukuoikeudet seuraaviin koulutusohjelmiin: ',
-    en: 'You are claiming read-permissions for the following studyprogrammes: ',
-    se: '',
   },
   buttonText: {
     fi: 'Vastaanota',
@@ -46,7 +41,8 @@ const translations = {
       'Virhe: Käyttämäsi linkki ei ole enää voimassa. Ole hyvä ja ota yhteys koulutusohjelmasi johtajaan uutta linkkiä varten.',
     en:
       'Error: The url you tried to access in no longer valid. Please contact your study programme leader for a new one.',
-    se: 'Fel: Länken du använde gäller inte längre. Vänligen kontakta ledaren för ditt utbildningsprogram för att få en ny länk.',
+    se:
+      'Fel: Länken du använde gäller inte längre. Vänligen kontakta ledaren för ditt utbildningsprogram för att få en ny länk.',
   },
   claimPermissions: {
     en: 'Form - Claim permissions',
@@ -122,31 +118,59 @@ export default ({ url }) => {
 
   if (!token.data || !faculties) return <Loader active inline />
 
-  const getFacultyMessageContent = () => {
+  const getProgrammeNames = () => {
     const programeCodes = faculties.find((f) => f.code === token.data.faculty).programmes
     const localizedProgrammeCodes = programeCodes.map((pCode) => {
       const prog = studyProgrammes.find((p) => p.key === pCode)
       return prog.name[languageCode]
     })
-    return translations.promptFaculty[languageCode] + localizedProgrammeCodes.join(', ')
+
+    return localizedProgrammeCodes
+  }
+
+  if (token.data.programme) {
+    return (
+      <div style={{ width: '50em', margin: '1em auto' }}>
+        <Message color="blue" icon="exclamation" content={translations.prompt[languageCode]} />
+        <div style={{ fontWeight: 'bold' }}>{localizedProgramname}</div>
+        <div
+          style={{ fontSize: '1.5em', fontWeight: 'bolder', height: '1.25em', margin: '0.5em 0' }}
+        >
+          <Icon color="blue" name={labelIcon[token.data.type]} size="small" />{' '}
+          {translations.rights[token.data.type][languageCode]}
+        </div>
+        {token.data.type === 'ADMIN' && (
+          <div style={{ margin: '1.2em 0' }}>
+            <Input
+              style={{ width: '700px' }}
+              size="large"
+              placeholder={localizedProgramname}
+              value={value}
+              onChange={(e, { value }) => setValue(value)}
+            />
+            <div
+              className={`claimAccesspage-adminMessage ${buttonIsDisabled() ? 'error' : 'valid'}`}
+            >
+              {translations.confirmPrompt[languageCode]}
+            </div>
+          </div>
+        )}
+        <Button disabled={buttonIsDisabled()} onClick={() => handleClaim(token.data)}>
+          {translations.buttonText[languageCode]}
+        </Button>{' '}
+      </div>
+    )
   }
 
   return (
     <div style={{ width: '50em', margin: '1em auto' }}>
-      <Message
-        color="blue"
-        icon="exclamation"
-        content={
-          token.data.programme ? translations.prompt[languageCode] : getFacultyMessageContent()
-        }
-      />
-      {token.data.programme ? (
-        <div style={{ fontWeight: 'bold' }}>{localizedProgramname}</div>
-      ) : (
-        <div style={{ fontWeight: 'bold' }}>
-          {faculties.find((f) => f.code === token.data.faculty).name}
-        </div>
-      )}
+      <Message color="blue" icon="exclamation" content={translations.prompt[languageCode]} />
+      <h2>{faculties.find((f) => f.code === token.data.faculty).name}</h2>
+      <List bulleted>
+        {getProgrammeNames().map((name) => (
+          <List.Item key={name}>{name}</List.Item>
+        ))}
+      </List>
       <div style={{ fontSize: '1.5em', fontWeight: 'bolder', height: '1.25em', margin: '0.5em 0' }}>
         <Icon color="blue" name={labelIcon[token.data.type]} size="small" />{' '}
         {translations.rights[token.data.type][languageCode]}
