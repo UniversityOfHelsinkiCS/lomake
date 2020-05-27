f654e624362a7df4469fc240d09352fbda66a779
Marko Koskinen <markokoskinen2037@gmail.com>
2020-05-27 16:25:48 +0300

    new logging scheme

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const morgan = require('morgan')
 const routes = require('@util/routes')
 const { inProduction } = require('@util/common')
 const errorMiddleware = require('@middleware/errorMiddleware')
@@ -6,6 +7,30 @@ const shibbolethCharsetMiddleware = require('@middleware/shibbolethCharsetMiddle
 const userMiddleware = require('@middleware/userMiddleware')
 const requestLoggerMiddleware = require('@middleware/requestLoggerMiddleware')
 const currentUserMiddleware = require('@middleware/currentUserMiddleware')
+const logger = require('@util/logger')
+
+const accessLogger = morgan((tokens, req, res) => {
+  const fields = [
+    'method',
+    'url',
+    'status',
+    'response-time',
+    'remote-addr',
+    'remote-user',
+    'user-agent',
+    'referrer',
+  ]
+
+  const final = {
+    userId: req.headers.uid,
+    method: tokens.method(req, res),
+    url: tokens.url(req, res),
+    status: tokens.status(req, res),
+    responseTime: tokens['response-time'](req, res),
+  }
+
+  logger.info(JSON.stringify(final))
+})
 
 const app = express()
 
@@ -14,7 +39,7 @@ app.use(express.json({ limit: '50mb' }))
 app.use(shibbolethCharsetMiddleware)
 app.use(userMiddleware)
 app.use(currentUserMiddleware)
-if (!inProduction) app.use(requestLoggerMiddleware)
+app.use(accessLogger)
 
 app.use(routes)
 
