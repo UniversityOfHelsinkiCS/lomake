1b5908c487d1f61c0b9fa93741087ea916988428
Marko Koskinen <markokoskinen2037@gmail.com>
2020-05-28 14:23:28 +0300

    fix production messages not being accepted by graylog

--- server/util/logger.js
+++ server/util/logger.js
@@ -3,10 +3,33 @@ const morgan = require('morgan')
 const { inProduction } = require('@util/common')
 
 /**
+ * Levels from Winston's documentation
+ * https://github.com/winstonjs/winston
+ */
+const levels = {
+  error: 0,
+  warn: 1,
+  info: 2,
+  http: 3,
+  verbose: 4,
+  debug: 5,
+  silly: 6,
+}
+
+/**
  * Production logger.
  */
+const myFormat = winston.format.printf(({ message, level }) => {
+  const temp = {
+    level: levels[level], // Send integer value. Graylog does not accept strings i.e. "info"
+    message,
+  }
+
+  return JSON.stringify(temp)
+})
+
 const productionLogger = winston.createLogger({
-  format: winston.format.json(),
+  format: myFormat,
   transports: new winston.transports.Console(),
 })
 
