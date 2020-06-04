75a3e10118928ba4d2bccce3bb4ee4762cadde32
Marko Koskinen <markokoskinen2037@gmail.com>
2020-06-04 17:19:17 +0300

    allow sorting users

--- client/components/UsersPage/UserTable.js
+++ client/components/UsersPage/UserTable.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
-import { Grid, Header, Segment } from 'semantic-ui-react'
+import { Grid, Header, Icon } from 'semantic-ui-react'
 import { useHistory } from 'react-router'
 import User from 'Components/UsersPage/User'
 import { isSuperAdmin } from '../../../config/common'
 
 export default () => {
+  const [sorter, setSorter] = useState('email')
+  const [reverse, setReverse] = useState(false)
+
   const users = useSelector((state) => state.users.data)
   const user = useSelector(({ currentUser }) => currentUser.data)
   const history = useHistory()
@@ -16,34 +19,47 @@ export default () => {
 
   if (!users) return null
 
+  let sortedUsers = users.sort((a, b) => {
+    if (typeof a[sorter] === 'string') return a[sorter].localeCompare(b[sorter])
+    if (typeof a[sorter] === 'boolean') return a[sorter] - b[sorter]
+  })
+
+  if (reverse) sortedUsers.reverse()
+
+  const CustomHeader = ({ width, name, field, sortable = true }) => {
+    const sortHandler = sortable
+      ? () => {
+          if (sorter === field) {
+            setReverse(!reverse)
+          } else {
+            setSorter(field)
+          }
+        }
+      : undefined
+    const style = sortable ? { cursor: 'pointer' } : {}
+
+    return (
+      <Grid.Column style={{ display: 'flex', alignItems: 'baseline' }} width={width}>
+        <Header onClick={sortHandler} style={style} as="h4">
+          {name}
+        </Header>
+        {sortable && <Icon name="sort" />}
+      </Grid.Column>
+    )
+  }
+
   return (
     <Grid celled="internally">
       <Grid.Row>
-        <Grid.Column width={3}>
-          <Header as="h4">Name</Header>
-        </Grid.Column>
-        <Grid.Column width={2}>
-          <Header as="h4">User id</Header>
-        </Grid.Column>
-        <Grid.Column width={3}>
-          <Header as="h4">Email</Header>
-        </Grid.Column>
-        <Grid.Column width={5}>
-          <Header as="h4">Access</Header>
-        </Grid.Column>
-        <Grid.Column width={1}>
-          <Header as="h4">Admin</Header>
-        </Grid.Column>
-        <Grid.Column width={1}>
-          <Header as="h4">Hide</Header>
-        </Grid.Column>
-        {isSuperAdmin(user.uid) && (
-          <Grid.Column width={1}>
-            <Header as="h4">Hijack</Header>
-          </Grid.Column>
-        )}
+        <CustomHeader width={3} name="Name" field="lastname" />
+        <CustomHeader width={2} name="User id" field="uid" />
+        <CustomHeader width={3} name="Email" field="email" />
+        <CustomHeader width={5} name="Access" field="access" sortable={false} />
+        <CustomHeader width={1} name="Admin" field="admin" />
+        <CustomHeader width={1} name="Hide" field="irrelevant" />
+        {isSuperAdmin(user.uid) && <CustomHeader width={1} name="Hijack" sortable={false} />}
       </Grid.Row>
-      {users.map((u) => (
+      {sortedUsers.map((u) => (
         <User user={u} key={u.id} />
       ))}
     </Grid>
