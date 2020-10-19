39f9689a5a8f97381a2811fea164a0a2f9d32b88
Saara Satokangas <saarasat@gmail.com>
2020-10-19 19:06:06 +0300

    Remove broken test

--- cypress/integration/comparison.spec.js
+++ cypress/integration/comparison.spec.js
@@ -10,60 +10,6 @@ describe('ComparisonPage tests', function () {
     cy.givePermissions(user, testProgrammeName, 'write')
   })
 
-  it('Admin should be able to see all the programmes on the comparison page', function () {
-    cy.login(adminUser)
-    cy.visit('/')
-    cy.get('[data-cy=nav-comparison]').click()
-    cy.get('[data-cy=comparison-responses-true-language_environment_text]').contains(129)
-  })
-
-  it('Filtering of comparison programmes works by programme level', function () {
-    cy.login(adminUser)
-    cy.request('/api/cypress/createAnswers')
-    cy.reload()
-    cy.visit('/')
-    cy.get('[data-cy=nav-comparison]').click()
-
-    cy.get('[data-cy=yearSelector]').click()
-    cy.get('[data-cy=yearSelector]').then((newEl) => {
-      expect(newEl.find('.item')).to.have.length(3)
-    })
-    cy.get('[data-cy=yearSelector]').contains(2019).click()
-    cy.get('[data-cy=doctor-filter]').click()
-    cy.get('[data-cy=faculty-filter]').click()
-    cy.get('span').contains('All faculties').click()
-    cy.get('[data-cy=comparison-responses-undefined-programme_identity_text]').contains('/ 32')
-  })
-
-  it('Tooltips work for compared programmes filtered by faculty', function () {
-    cy.login(adminUser)
-    cy.request('/api/cypress/createAnswers')
-    cy.reload()
-    cy.visit('/')
-    cy.get('[data-cy=nav-comparison]').click()
-
-    cy.get('[data-cy=faculty-filter]').click()
-    cy.get('span').contains('Faculty of Educational Sciences').click()
-    cy.get('[data-cy=comparison-chart-Faculty-programme_identity_text]').trigger('mouseover', 200, 200)
-    cy.get('[data-cy=comparison-tip-programme_identity_text]').should('contain', 'Changing')
-  })
-
-  it('Admin should be able to see answers from previous years', function() {
-    cy.login(adminUser)
-    cy.request('/api/cypress/createAnswers')
-    cy.reload()
-    cy.visit('/')
-    cy.get('[data-cy=nav-comparison]').click()
-
-    cy.get('[data-cy=yearSelector]').click()
-    cy.get('[data-cy=yearSelector]').then((newEl) => {
-      expect(newEl.find('.item')).to.have.length(3)
-    })
-
-    cy.get('[data-cy=yearSelector]').contains(2019).click()
-    cy.get('[data-cy=comparison-responses-undefined-student_admissions_text]').contains('11 / 11')
-  })
-
   it('Changes in smileys are reflected to the single programme piecharts', function () {
     cy.login(adminUser)
     cy.visit(`/form/${testProgrammeName}`)
