a3c396ef3e1569634609c3ce57513de540b96638
Kalle (Sendou) <38327916+Sendouc@users.noreply.github.com>
2020-11-05 19:19:40 +0200

    remove useless import

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { Dropdown, Icon, Label, Menu } from 'semantic-ui-react'
