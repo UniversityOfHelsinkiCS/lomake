3c296e45e74c8dcf4cc1713e9e297ad784c23072
Marko K <markokoskinen2037@gmail.com>
2020-11-10 15:17:34 +0200

    Fix testerino

--- cypress/integration/yearselector.spec.js
+++ cypress/integration/yearselector.spec.js
@@ -12,11 +12,10 @@ describe("Previous year's answers", function () {
   })
 
   it('If no answers for previousyears, cant change year', function () {
-    cy.get('[data-cy=yearSelector]').should('be.visible')
-    cy.get('[data-cy=yearSelector]').then((el) => {
-      el.click()
-      expect(el.find('.item')).to.have.length(1)
-    })
+    cy.get('[data-cy=yearSelector]').should(
+      'have.class',
+      'disabled'
+    )
   })
 
   it("Can switch which year's answers to see in OverViewPage", function () {
