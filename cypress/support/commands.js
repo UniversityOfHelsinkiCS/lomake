24291007e58429af1535da4fd7d6070f80969708
Marko Koskinen <markokoskinen2037@gmail.com>
2020-12-18 14:10:08 +0200

    Small enhancement to cypress's custom commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,35 +1,14 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+/* eslint-disable no-undef */
+/// <reference types="cypress" />
+
+import { setHeaders } from "../../client/util/mockHeaders"
 
 /**
  * Logs in as specified user.
  * @param {string} uid Uid of user to login as
  */
 Cypress.Commands.add('login', (uid) => {
-  window.localStorage.setItem('fakeUser', JSON.stringify({ uid }))
+  setHeaders(uid)
   cy.log('Logged in as', uid)
 })
 
