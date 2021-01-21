7627fce8511a8ea73040b242d85810f2203b8760
Saara Satokangas <saarasat@gmail.com>
2021-01-21 13:26:06 +0200

    Disable IAM-checking for now

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const routes = require('@util/routes')
-const { inProduction } = require('@util/common')
+const { inProduction, AUTOMATIC_IAM_PERMISSIONS_ENABLED } = require('@util/common')
+const logger = require('@util/logger')
+
 const errorMiddleware = require('@middleware/errorMiddleware')
 const shibbolethCharsetMiddleware = require('@middleware/shibbolethCharsetMiddleware')
 const {
@@ -18,7 +20,12 @@ app.use(express.json({ limit: '50mb' }))
 app.use(inProduction ? productionRequestLogger : developmentRequestLogger)
 app.use(shibbolethCharsetMiddleware)
 app.use(userMiddleware)
-app.use(IAMmiddleware)
+
+if (AUTOMATIC_IAM_PERMISSIONS_ENABLED) {
+  logger.info('Automatic IAM permissions enabled')
+  app.use(IAMmiddleware)
+}
+
 app.use(currentUserMiddleware)
 
 app.use(routes)
--- server/util/common.js
+++ server/util/common.js
@@ -2,9 +2,11 @@ const common = require('@root/config/common')
 
 const DB_URL = process.env.DB_URL || ''
 const PORT = process.env.PORT || 8000
+const AUTOMATIC_IAM_PERMISSIONS_ENABLED = process.env.AUTOMATIC_IAM_PERMISSIONS_ENABLED === "true"
 
 module.exports = {
   ...common,
   DB_URL,
   PORT,
+  AUTOMATIC_IAM_PERMISSIONS_ENABLED
 }
