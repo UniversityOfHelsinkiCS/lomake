578af0de9521c9d157325cb4a1c0951d315518d1
Saara Satokangas <saarasat@gmail.com>
2021-08-09 13:36:18 +0300

    Move access handling into its own component

--- client/components/UsersPage/AccessModal.js
+++ client/components/UsersPage/AccessModal.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
-import { Button, Form, Icon, Label, Popup, Radio, Segment, Table } from 'semantic-ui-react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Button, Form, Label, Popup, Radio, Segment } from 'semantic-ui-react'
 
+import AccessTable from './AccessTable'
 import CustomModal from '../Generic/CustomModal'
 import ProgrammeFilter from 'Components/Generic/ProgrammeFilter'
 import { isSuperAdmin } from '../../../config/common'
@@ -15,36 +16,16 @@ const AccessModal = ({
   handleSearch,
   programmeFilter,
   onEmpty,
-  lang,
   filteredProgrammes,
   programmeCodesAndNames
 }) => {
   const dispatch = useDispatch()
-  if (!user || user.pending) return null
+  const lang = useSelector((state) => state.language)
 
+  if (!user || user.pending) return null
 
   const deleteUser = () => {
     dispatch(deleteUserAction(user.id))
-  }
-
-  const grantOwner = (programme) => {
-    const updatedUser = { id: user.id, access: { ...user.access, [programme]: { admin: true, read: true, write: true }}}
-    dispatch(editUserAction(updatedUser))
-  }
-
-  const removeOwner = (programme) => {
-    const updatedUser = { id: user.id, access: { ...user.access, [programme]: { ...user.access[programme], admin: false }}}
-    dispatch(editUserAction(updatedUser))
-  }
-
-  const grantEdit = (programme) => {
-    const updatedUser = { id: user.id, access: { ...user.access, [programme]: { ...user.access[programme], write: true }}}
-    dispatch(editUserAction(updatedUser))
-  }
-
-  const removeEdit = (programme) => {
-    const updatedUser = { id: user.id, access: { ...user.access, [programme]: { ...user.access[programme], write: false, admin: false }}}
-    dispatch(editUserAction(updatedUser))
   }
 
   const grantView = (programme) => {
@@ -52,48 +33,14 @@ const AccessModal = ({
     dispatch(editUserAction(updatedUser))
   }
 
-  const removeView = (programme) => {
-    let userObject = user
-    delete userObject.access[programme]
-    const updatedUser = { id: user.id, access: userObject.access }
-    dispatch(editUserAction(updatedUser))
-  }
-
-  const getSwitchableBadge = ({ cyTag, currentAccess, grant, remove, disabled = false }) => {
-    if (currentAccess)
-      return (
-        <Popup
-          trigger={<Icon data-cy={cyTag} name="check" className="users-green" size="large" />}
-          content={
-            <Button
-              data-cy="removePermissions-button"
-              disabled={disabled}
-              color="red"
-              content={translations.removeAccess[lang]}
-              onClick={() => remove()}
-            />
-          }
-          on="click"
-          position="top center"
-        />
-      )
-    return (
-      <Icon
-        data-cy={cyTag}
-        name="close"
-        className="users-red"
-        size="large"
-        onClick={() => grant()}
-      />
-    )
-  }
-
   const grantAdmin = () => {
     // Removed wideReadAccess, because we dont want users to have two usergroups. (admin and wideReadAccess)
     dispatch(editUserAction({ id: user.id, admin: true, wideReadAccess: false }))
   }
 
-  const removeAdmin = () => dispatch(editUserAction({ id: user.id, admin: false }))
+  const removeAdmin = () => {
+    dispatch(editUserAction({ id: user.id, admin: false }))
+  }
 
   const CustomRadioWithConfirmTrigger = ({
     checked,
@@ -220,75 +167,12 @@ const AccessModal = ({
       </Segment>
       <Segment className="user-access-modal-segment">
 
-      {user.access && Object.keys(user.access).length > 0 &&
-        <Table className="user-access-modal-segment">
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell width={5}>
-                {translations.programme[lang]}
-              </Table.HeaderCell>
-              <Table.HeaderCell width={3}>
-                {translations.readAccess[lang]}
-              </Table.HeaderCell>
-              <Table.HeaderCell width={3}>
-                {translations.writeAccess[lang]}
-              </Table.HeaderCell>
-              <Table.HeaderCell width={3}>
-                {translations.ownerAccess[lang]}
-              </Table.HeaderCell>
-              <Table.HeaderCell />
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            {Object.keys(user.access).map((programme) => (
-              <Table.Row key={`${user.lastname}-${programme}`}>
-                <Table.Cell>
-                  {programmeCodesAndNames.get(programme)}
-                </Table.Cell>
-                <Table.Cell>
-                {getSwitchableBadge({
-                  cyTag: `read-${user.uid}`,
-                  currentAccess: user.access[programme] ? user.access[programme].read : false,
-                  grant: () => grantView(programme),
-                  remove: () => removeView(programme)
-                })}
-                </Table.Cell>
-                <Table.Cell>
-                {getSwitchableBadge({
-                  cyTag: `write-${user.uid}`,
-                  currentAccess: user.access[programme] ? user.access[programme].write : false,
-                  grant: () => grantEdit(programme),
-                  remove: () => removeEdit(programme)
-                })}
-                </Table.Cell>
-                <Table.Cell>
-                {getSwitchableBadge({
-                  cyTag: `read-${user.uid}`,
-                  currentAccess: user.access[programme] ? user.access[programme].admin : false,
-                  grant: () => grantOwner(programme),
-                  remove: () => removeOwner(programme)
-                })}
-                </Table.Cell>
-                <Table.Cell>
-                <Popup
-                  trigger={<Button>{translations.removeAccessToProgramme[lang]}</Button>}
-                  content={
-                    <Button
-                      data-cy="removePermissions-button"
-                      color="red"
-                      content={translations.removeAllAccess[lang]}
-                      onClick={() => removeView(programme)}
-                    />
-                  }
-                  on="click"
-                  position="top center"
-                />
-                </Table.Cell>
-              </Table.Row>
-            ))}
-          </Table.Body>
-        </Table>
-      }
+      {user.access && Object.keys(user.access).length > 0 && (
+        <AccessTable
+          user={user}
+          programmeCodesAndNames={programmeCodesAndNames}
+        />
+      )}
         <h3>{translations.addRights[lang]}</h3>
         <div >
           <ProgrammeFilter
---
+++ client/components/UsersPage/AccessTable.js
@@ -0,0 +1,145 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Button, Icon, Popup, Table } from 'semantic-ui-react'
+
+import { editUserAction } from 'Utilities/redux/usersReducer'
+import { usersPageTranslations as translations } from 'Utilities/translations'
+
+
+const AccessTable = ({ user, programmeCodesAndNames}) => {
+  const dispatch = useDispatch()
+  const lang = useSelector((state) => state.language)
+
+  const grantOwner = (programme) => {
+    const updatedUser = { id: user.id, access: { ...user.access, [programme]: { admin: true, read: true, write: true }}}
+    dispatch(editUserAction(updatedUser))
+  }
+
+  const removeOwner = (programme) => {
+    const updatedUser = { id: user.id, access: { ...user.access, [programme]: { ...user.access[programme], admin: false }}}
+    dispatch(editUserAction(updatedUser))
+  }
+
+  const grantEdit = (programme) => {
+    const updatedUser = { id: user.id, access: { ...user.access, [programme]: { ...user.access[programme], write: true }}}
+    dispatch(editUserAction(updatedUser))
+  }
+
+  const removeEdit = (programme) => {
+    const updatedUser = { id: user.id, access: { ...user.access, [programme]: { ...user.access[programme], write: false, admin: false }}}
+    dispatch(editUserAction(updatedUser))
+  }
+
+  const grantView = (programme) => {
+    const updatedUser = { id: user.id, access: { ...user.access, [programme]: { ...user.access[programme], read: true }}}
+    dispatch(editUserAction(updatedUser))
+  }
+
+  const removeView = (programme) => {
+    let userObject = user
+    delete userObject.access[programme]
+    const updatedUser = { id: user.id, access: userObject.access }
+    dispatch(editUserAction(updatedUser))
+  }
+
+  const getSwitchableBadge = ({ cyTag, currentAccess, grant, remove, disabled = false }) => {
+    if (currentAccess)
+      return (
+        <Popup
+          trigger={<Icon data-cy={cyTag} name="check" className="users-green" size="large" />}
+          content={
+            <Button
+              data-cy="removePermissions-button"
+              disabled={disabled}
+              color="red"
+              content={translations.removeAccess[lang]}
+              onClick={() => remove()}
+            />
+          }
+          on="click"
+          position="top center"
+        />
+      )
+    return (
+      <Icon
+        data-cy={cyTag}
+        name="close"
+        className="users-red"
+        size="large"
+        onClick={() => grant()}
+      />
+    )
+  }
+
+  return (
+    <Table className="user-access-modal-segment">
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell width={5}>
+            {translations.programme[lang]}
+          </Table.HeaderCell>
+          <Table.HeaderCell width={3}>
+            {translations.readAccess[lang]}
+          </Table.HeaderCell>
+          <Table.HeaderCell width={3}>
+            {translations.writeAccess[lang]}
+          </Table.HeaderCell>
+          <Table.HeaderCell width={3}>
+            {translations.ownerAccess[lang]}
+          </Table.HeaderCell>
+          <Table.HeaderCell />
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {Object.keys(user.access).map((programme) => (
+          <Table.Row key={`${user.lastname}-${programme}`}>
+            <Table.Cell>
+              {programmeCodesAndNames.get(programme)}
+            </Table.Cell>
+            <Table.Cell>
+            {getSwitchableBadge({
+              cyTag: `read-${user.uid}`,
+              currentAccess: user.access[programme] ? user.access[programme].read : false,
+              grant: () => grantView(programme),
+              remove: () => removeView(programme)
+            })}
+            </Table.Cell>
+            <Table.Cell>
+            {getSwitchableBadge({
+              cyTag: `write-${user.uid}`,
+              currentAccess: user.access[programme] ? user.access[programme].write : false,
+              grant: () => grantEdit(programme),
+              remove: () => removeEdit(programme)
+            })}
+            </Table.Cell>
+            <Table.Cell>
+            {getSwitchableBadge({
+              cyTag: `read-${user.uid}`,
+              currentAccess: user.access[programme] ? user.access[programme].admin : false,
+              grant: () => grantOwner(programme),
+              remove: () => removeOwner(programme)
+            })}
+            </Table.Cell>
+            <Table.Cell>
+            <Popup
+              trigger={<Button>{translations.removeAccessToProgramme[lang]}</Button>}
+              content={
+                <Button
+                  data-cy="removePermissions-button"
+                  color="red"
+                  content={translations.removeAllAccess[lang]}
+                  onClick={() => removeView(programme)}
+                />
+              }
+              on="click"
+              position="top center"
+            />
+            </Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table>
+  )  
+}
+
+export default AccessTable
