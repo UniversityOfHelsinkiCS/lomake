899bd5645b69ae39fe061d4d022cab6af93a6d36
Saara Satokangas <saarasat@gmail.com>
2021-08-13 16:02:03 +0300

    Assign max-height to the custom modal

--- client/components/Generic/CustomModal.js
+++ client/components/Generic/CustomModal.js
@@ -10,7 +10,7 @@ const CustomModal = ({ children, title, closeModal, borderColor }) => {
     <div className="customModal-dimmer">
       <div
         className="customModal-content"
-        style={borderColor ? { border: '3px solid', borderColor } : {}}
+        style={borderColor ? { border: '3px solid', borderColor, maxHeight: '90%' } : { maxHeight: '90%' }}
         ref={ref}
       >
         <div className="title-and-close">
