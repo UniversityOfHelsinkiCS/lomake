053286529bd0f18016c62bd4086afe2782b162c9
Saara Satokangas <saarasat@gmail.com>
2022-01-12 11:46:22 +0200

    Add a test for the International Master's programmes user

--- cypress/integration/international2020.spec.js
+++ cypress/integration/international2020.spec.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-undef */
 /// <reference types="cypress" />
-import { defaultYears } from '../../config/common'
 
 const user = 'cypressInternationalUser'
 
@@ -12,6 +11,15 @@ describe('IAM permission tests', () => {
   it("User with access to International Master's programmes, should see 35 programmes on first page", () => {
     cy.visit('/')
     cy.get('[data-cy^=colortable-link-to]').should('have.have.length', 35)
+  })
+
+  it('User should not see only the data of 2020 in the form page', () => {
+    cy.visit('/')
+    // Master's programme in Data Science
+    cy.visit(`/form/MH50_010#0`)
+    cy.get('[data-cy=yearSelector]').should('have.class', 'disabled').contains(2020)
+    cy.get('[data-cy=textarea-learning_outcomes]').should('contain', 'Hello from 2020')
+    cy.get('[data-cy=locked-form-notice').should('not.contain', 'is open for')
   })
 
   it('Report works for 35 international programmes', () => {
