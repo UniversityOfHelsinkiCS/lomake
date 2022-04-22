d7a3e29ec01ebde0be9ce402c77d1bc158e17695
Veikko Suhonen <veikko.suhonen@helsinki.fi>
2022-04-22 13:04:40 +0300

    Add back hyGroup parsing

--- server/middleware/IAMmiddleware.js
+++ server/middleware/IAMmiddleware.js
@@ -1,10 +1,24 @@
 const { inProduction } = require('@util/common')
+const logger = require('@util/logger')
+
+const parseHyGroups = hyGroups => {
+  let parsedHyGroups = []
+  if (!(hyGroups === undefined || hyGroups === '' || hyGroups === null)) {
+    parsedHyGroups = hyGroups.split(';')
+  }
+  return parsedHyGroups
+}
 
 const IAMmiddleware = async (req, res, next) => {
   if (req.path.includes('socket.io')) return next()
   if (!inProduction && req.path.includes('/cypress/')) return next()
 
   const { user } = req
+  const { headers } = req
+
+  user.hyGroups = parseHyGroups(headers?.hygroupcn)
+
+  logger.info(`User ${user.uid} logged in with groups ${user.hyGroups}`)
 
   next()
 }
