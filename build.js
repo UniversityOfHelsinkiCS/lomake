5dee944de27082f04a14caeaf979c8cc70b35ec2
Saara Satokangas <saarasat@gmail.com>
2022-05-04 13:27:22 +0300

    Remove logger from build.js

--- build.js
+++ build.js
@@ -1,15 +1,12 @@
 const { sassPlugin } = require('esbuild-sass-plugin')
-const logger = require('@util/logger')
 
-require('esbuild')
-  .build({
-    entryPoints: ['client/index.js'],
-    loader: { '.js': 'jsx', '.png': 'dataurl', '.svg': 'dataurl', '.jpg': 'dataurl' },
-    bundle: true,
-    minify: true,
-    outdir: 'dist',
-    define: { 'process.env.NODE_ENV': "'production'", 'process.env.ENVIRONMENT': "'production'", global: 'window' },
-    plugins: [sassPlugin()],
-    color: true,
-  })
-  .then(() => logger.info('Build successful'))
+require('esbuild').build({
+  entryPoints: ['client/index.js'],
+  loader: { '.js': 'jsx', '.png': 'dataurl', '.svg': 'dataurl', '.jpg': 'dataurl' },
+  bundle: true,
+  minify: true,
+  outdir: 'dist',
+  define: { 'process.env.NODE_ENV': "'production'", 'process.env.ENVIRONMENT': "'production'", global: 'window' },
+  plugins: [sassPlugin()],
+  color: true,
+})
