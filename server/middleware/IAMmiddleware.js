1f15783b6800a4c702b57a42f45ee457c9568c11
Saara Satokangas <saarasat@gmail.com>
2022-06-04 21:10:17 +0300

    Remove unnecessary inProduction-flags

--- server/middleware/IAMmiddleware.js
+++ server/middleware/IAMmiddleware.js
@@ -1,15 +1,14 @@
-const { inProduction, AUTOMATIC_IAM_PERMISSIONS_ENABLED } = require('@util/common')
+const { AUTOMATIC_IAM_PERMISSIONS_ENABLED } = require('@util/common')
 const { getIAMRights } = require('@util/IAMrights')
 
 const IAMmiddleware = async (req, _, next) => {
   if (req.path.includes('socket.io')) return next()
-  if (!inProduction && req.path.includes('/cypress/')) return next()
+  if (req.path.includes('/cypress/')) return next()
 
   const { user } = req
   const { headers } = req
 
-  if (!inProduction || AUTOMATIC_IAM_PERMISSIONS_ENABLED) {
-    // testing iam group based access granting
+  if (AUTOMATIC_IAM_PERMISSIONS_ENABLED) {
     const { newAccess, newSpecialGroup } = getIAMRights(headers?.hygroupcn)
     user.access = newAccess
     user.specialGroup = newSpecialGroup
