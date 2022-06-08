af19a0bc5296b48e86c3f6382741c05642105153
Saara Satokangas <saarasat@gmail.com>
2022-06-08 16:11:52 +0300

    Fix ordering by lastLogin-date in user-table

--- client/components/UsersPage/UserTable.js
+++ client/components/UsersPage/UserTable.js
@@ -158,8 +158,13 @@ export default () => {
             {getCustomHeader({ name: translations.userId[lang], width: 1, field: 'uid' })}
             {getCustomHeader({ name: translations.access[lang], width: 6, field: 'access', sortable: true })}
             {getCustomHeader({ name: translations.userGroup[lang], width: 2, field: 'userGroup' })}
-            {getCustomHeader({ name: translations.lastLogin[lang], width: 2, field: 'specialGroup', sortable: true })}
-            {getCustomHeader({ name: translations.specialGroup[lang], width: 2, field: 'lastLogin', sortable: true })}
+            {getCustomHeader({ name: translations.lastLogin[lang], width: 2, field: 'lastLogin', sortable: true })}
+            {getCustomHeader({
+              name: translations.specialGroup[lang],
+              width: 2,
+              field: 'specialGroup',
+              sortable: true,
+            })}
             {!iamsInUse &&
               getCustomHeader({ name: translations.editUser[lang], width: 1, field: 'editUser', sortable: false })}
             {isSuperAdmin(user) && getCustomHeader({ name: 'Hijack', width: 1, field: 'deleteUser', sortable: false })}
