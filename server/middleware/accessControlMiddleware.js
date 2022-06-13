79811d72beb062de229dce53528c571f1930d2d5
Laura Sinikallio <46318664+sinikala@users.noreply.github.com>
2022-06-13 15:35:09 +0300

    Remove returns because eslint

--- server/middleware/accessControlMiddleware.js
+++ server/middleware/accessControlMiddleware.js
@@ -6,21 +6,21 @@ const requireProgrammeRead = (req, res, next) => {
   const { programme } = req.params
   if (isAdmin(req.user) || isSuperAdmin(req.user)) next()
   else if (req.user.access[programme] && req.user.access[programme].read) next()
-  else return res.status(401).json({ error: 'Unauthorized access.' }).end()
+  else res.status(401).json({ error: 'Unauthorized access.' }).end()
 }
 
 const requireProgrammeWrite = (req, res, next) => {
   const { programme } = req.params
   if (isAdmin(req.user) || isSuperAdmin(req.user)) next()
   else if (req.user.access[programme] && req.user.access[programme].write) next()
-  else return res.status(401).json({ error: 'Unauthorized access.' }).end()
+  else res.status(401).json({ error: 'Unauthorized access.' }).end()
 }
 
 const requireProgrammeOwner = (req, res, next) => {
   const { programme } = req.params
   if (isAdmin(req.user) || isSuperAdmin(req.user)) next()
   else if (req.user.access[programme] && req.user.access[programme].admin) next()
-  else return res.status(401).json({ error: 'Unauthorized access.' }).end()
+  else res.status(401).json({ error: 'Unauthorized access.' }).end()
 }
 
 const checkAdmin = (req, res, next) => {
@@ -36,7 +36,7 @@ const notInProduction = (req, res, next) => {
     next()
   } else {
     logger.error(`Test-only route (${req.method} ${req.url}) was requested while in production mode.`)
-    return res.status(404).end()
+    res.status(404).end()
   }
 }
 
