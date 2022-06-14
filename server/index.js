1b9f3d33fc9ea03e7bde5787731f5bb2dda38be7
Saara Satokangas <saarasat@gmail.com>
2022-06-14 11:12:59 +0300

    Remove external router

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const routes = require('@util/routes')
-const externalRouter = require('@util/externalRoutes')
 const { AUTOMATIC_IAM_PERMISSIONS_ENABLED } = require('@util/common')
 const logger = require('@util/logger')
 
@@ -16,8 +15,6 @@ const app = express()
 app.use(express.json({ limit: '50mb' }))
 
 app.use(accessLogger)
-
-app.use('/external', externalRouter)
 
 app.use(shibbolethCharsetMiddleware)
 app.use(userMiddleware)
