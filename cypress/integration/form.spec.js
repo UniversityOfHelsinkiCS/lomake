18cef05b6f48fe6b22d74bdf5c9ec85016c76a5e
Saara Satokangas <saarasat@gmail.com>
2022-07-28 19:58:07 +0300

    Try changing the test browser to chrome

--- cypress/integration/form.spec.js
+++ cypress/integration/form.spec.js
@@ -16,10 +16,12 @@ describe('Form tests', () => {
   it('Can write to a textfield and the answer is saved.', () => {
     cy.get('[data-cy=yearSelector]').contains(defaultYears[1])
     cy.get('[data-cy=textarea-review_of_last_years_situation_report]').find('.editor-class').click()
-    cy.writeToTextField('[contenteditable="true"]', 'kissa')
+    cy.writeToTextField('[contenteditable="true"]', 'other words')
     cy.reload()
 
-    cy.get('[data-cy=textarea-review_of_last_years_situation_report]').should('contain.text', 'kissa')
+    cy.get('[data-cy=textarea-review_of_last_years_situation_report]')
+      .find('.editor-class')
+      .should('contain.text', 'other words')
   })
 
   it('Can open a question, click on smiley face, and the result it saved.', () => {
