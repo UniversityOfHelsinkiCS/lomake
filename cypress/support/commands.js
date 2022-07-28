6820c0c10b5401185661801b4acbccaf1db7454c
Saara Satokangas <saarasat@gmail.com>
2022-07-28 17:44:04 +0300

    Start typing at the end of the previous text

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -35,7 +35,7 @@ Cypress.Commands.add('copyToTextField', (editorName, textToBeTyped) => {
 })
 
 Cypress.Commands.add('writeToTextField', (editorName, textToBeTyped) => {
-  cy.get(editorName).type(textToBeTyped)
+  cy.get(editorName).type(`{moveToEnd}${textToBeTyped}`)
 })
 
 Cypress.Commands.add('getEditorInputLength', editorName => {
