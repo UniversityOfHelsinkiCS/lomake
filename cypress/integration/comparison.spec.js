e8ba3b6139f50facf82c9819fac55b497030f5f0
Saara Satokangas <saarasat@gmail.com>
2022-07-28 16:28:13 +0300

    Try fixing comparison page test

--- cypress/integration/comparison.spec.js
+++ cypress/integration/comparison.spec.js
@@ -17,14 +17,13 @@ describe('ComparisonPage tests', () => {
   it('Changes in smileys are reflected to the single programme piecharts', () => {
     cy.login(adminUser)
     cy.visit('/')
-    cy.selectYear(defaultYears[0])
+    cy.selectYear(defaultYears[1])
     cy.get(`[data-cy=colortable-link-to-${testProgrammeCode}]`).click()
     cy.get('[data-cy=color-neutral-review_of_last_years_situation_report]').click()
 
     cy.visit('/comparison')
     cy.get('[data-cy=programme-filter]').click()
     cy.get('span').contains(testProgrammeName).click()
-    cy.selectYear(defaultYears[0])
     cy.get('[data-cy=comparison-chart-review_of_last_years_situation_report_text')
     cy.get('path').should('have.css', 'stroke').and('eq', 'rgb(249, 208, 59)')
   })
