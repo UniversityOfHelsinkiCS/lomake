35c27e7265643b73734cc84c3edbdf9e1959d7b2
Laura Sinikallio <46318664+sinikala@users.noreply.github.com>
2022-07-29 13:10:13 +0300

    Fix cypress URL again

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:8000/tilannekuva',
+    baseUrl: 'http://localhost:8000/tilannekuva/',
     viewportWidth: 1500,
     viewportHeight: 1200,
     watchForFileChanges: false,
