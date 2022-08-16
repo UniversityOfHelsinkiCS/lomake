fd9e47fff16c7c9aae201ee9ddb3ddbdd1f17b85
Laura Sinikallio <46318664+sinikala@users.noreply.github.com>
2022-08-16 10:40:11 +0300

    Initialize Sentry

--- client/index.js
+++ client/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import * as Sentry from '@sentry/browser'
+import { BrowserTracing } from '@sentry/tracing'
 import { cypressUids, setHeaders } from '@root/config/mockHeaders'
 
 import './assets/custom.scss'
@@ -11,6 +13,16 @@ import { basePath } from 'Utilities/common'
 import App from 'Components/App'
 import ErrorBoundary from 'Components/ErrorBoundary'
 import './util/i18n'
+
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+  Sentry.init({
+    dsn: 'https://4ff51e515d0e41a28dea1bbcf3451a18@sentry.cs.helsinki.fi/11',
+    // environment: process.env.SENTRY_ENVIRONMENT,
+    // release: process.env.SENTRY_RELEASE,
+    integrations: [new BrowserTracing()],
+    tracesSampleRate: 1.0,
+  })
+}
 
 const refresh = () =>
   render(
@@ -23,6 +35,8 @@ const refresh = () =>
     </Provider>,
     document.getElementById('root')
   )
+// eslint-disable-next-line
+dothis()
 
 if (process.env.NODE_ENV === 'development') {
   const newUser = 'admin'
