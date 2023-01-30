587d3071f6f7f2b7d51f8bb528a5c9ef8c757a64
Laura Sinikallio <46318664+sinikala@users.noreply.github.com>
2023-01-30 11:44:41 +0200

    List katselmus questions in katselmus form

---
+++ client/components/KatselmusView/KatselmusFormView/KatselmusForm.js
@@ -0,0 +1,90 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import Textarea from 'Components/Generic/Textarea'
+import Entity from 'Components/Generic/Entity'
+import Measures from 'Components/Generic/Measures'
+import { colors, romanize } from 'Utilities/common'
+import Section from './KatselmusSection'
+
+const Form = ({ questions, programmeKey }) => {
+  const lang = useSelector(state => state.language)
+
+  const partComponentMap = {
+    TEXTAREA: Textarea,
+    ENTITY: Entity,
+    MEASURES: Measures,
+  }
+
+  let number = -1
+
+  const partMap = part => {
+    const summary =
+      part.id.includes('meta') || part.id.includes('information_needed') || part.id.includes('information_used')
+
+    const divStyle = summary
+      ? {
+          marginTop: '1em !important',
+          paddingLeft: '0.5em',
+          borderLeft: '5px solid',
+          borderColor: colors.background_black,
+          marginBottom: '0',
+        }
+      : {}
+
+    if (part.type === 'TITLE') {
+      return (
+        <h2 key={part.id} style={divStyle}>
+          {part.label[lang]}
+        </h2>
+      )
+    }
+
+    if (!Object.prototype.hasOwnProperty.call(partComponentMap, part.type)) {
+      return null
+    }
+
+    if (part.type === 'ENTITY' || part.type === 'MEASURES') number++
+
+    const Component = partComponentMap[part.type]
+    const description = part.description ? part.description[lang] : undefined
+    const extrainfo = part.extrainfo ? part.extrainfo[lang] : undefined
+    return (
+      <div key={part.id} style={divStyle}>
+        <Component
+          id={part.id}
+          label={part.label[lang]}
+          description={description}
+          required={part.required}
+          noColor={part.no_color}
+          number={number}
+          extrainfo={extrainfo}
+          previousYearsAnswers={null}
+        />
+      </div>
+    )
+  }
+
+  return (
+    <>
+      {questions.map((section, index) => {
+        return (
+          <Section
+            title={section.title[lang]}
+            number={romanize(index)}
+            key={section.title[lang]}
+            programmeKey={programmeKey}
+          >
+            {/* {section.link_title && section.link_url && (
+              <a className="hide-in-print-mode" target="_blank" href={section.link_url} rel="noreferrer">
+                {section.link_title[lang]}
+              </a>
+            )} */}
+            {section.parts.map(partMap)}
+          </Section>
+        )
+      })}
+    </>
+  )
+}
+
+export default Form
--- client/components/KatselmusView/KatselmusFormView/index.js
+++ client/components/KatselmusView/KatselmusFormView/index.js
@@ -1,23 +1,26 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import { Button } from 'semantic-ui-react'
 import { useTranslation } from 'react-i18next'
-import { Redirect } from 'react-router'
+import { Redirect, useHistory } from 'react-router'
 
 import { isAdmin } from '@root/config/common'
 import { colors } from 'Utilities/common'
 import { getProgramme } from 'Utilities/redux/studyProgrammesReducer'
 import NoPermissions from 'Components/Generic/NoPermissions'
 import NavigationSidebar from 'Components/FormView/NavigationSidebar'
+import calendarImage from 'Assets/calendar.jpg'
+import Form from './KatselmusForm'
 
 // import positiveEmoji from 'Assets/sunglasses.png'
 // import neutralEmoji from 'Assets/neutral.png'
 // import negativeEmoji from 'Assets/persevering.png'
 
-// import questions from '../../../katselmusQuestions.json'
+import questions from '../../../katselmusQuestions.json'
 
 const KatselmusFormView = ({ room }) => {
   const dispatch = useDispatch()
-  // const history = useHistory()
+  const history = useHistory()
   const { t } = useTranslation()
   const lang = useSelector(state => state.language)
   const user = useSelector(state => state.currentUser.data)
@@ -44,10 +47,19 @@ const KatselmusFormView = ({ room }) => {
     <div className="form-container">
       <NavigationSidebar programmeKey={room} katselmus />
       <div className="the-form">
-        <h1 style={{ color: colors.blue }}>{programme.name[lang]}</h1>
-        <h3 style={{ marginTop: '0' }} data-cy="formview-title">
-          {t('katselmus')} 2023
-        </h3>
+        <div className="form-instructions">
+          <div className="hide-in-print-mode">
+            <div style={{ marginBottom: '2em' }}>
+              <Button onClick={() => history.push('/')} icon="arrow left" />
+            </div>
+            <img alt="form-header-calendar" className="img-responsive" src={calendarImage} />
+          </div>
+          <h1 style={{ color: colors.blue }}>{programme.name[lang]}</h1>
+          <h3 style={{ marginTop: '0' }} data-cy="formview-title">
+            {t('katselmus')} 2023
+          </h3>
+        </div>
+        <Form programmeKey={programme.key} questions={questions} />
       </div>
     </div>
   )
