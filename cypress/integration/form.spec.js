2b50727166d81b597e75e20901cc3c7171bd3c43
Veikkosuhonen <veikko.suhonen@helsinki.fi>
2023-04-20 14:13:42 +0300

    Try a custom cy command for flaky text editors

--- cypress/integration/form.spec.js
+++ cypress/integration/form.spec.js
@@ -15,8 +15,9 @@ describe('Yearly assessment form tests', () => {
 
   it('Can write to a textfield and the answer is saved.', () => {
     cy.get('[data-cy=yearSelector]').contains(defaultYears[1])
-    cy.get('[data-cy=textarea-community_wellbeing]').find('.editor-class').click()
-    cy.get('[data-cy=textarea-community_wellbeing]').find('[contenteditable]').type('other words').wait(2000)
+    cy.typeInEditor('[data-cy=textarea-community_wellbeing]', 'other words')
+    // cy.get('[data-cy=textarea-community_wellbeing]').find('.editor-class').click()
+    // cy.get('[data-cy=textarea-community_wellbeing]').find('[contenteditable]').type('other words').wait(2000)
     cy.reload()
 
     cy.get('[data-cy=textarea-community_wellbeing]').find('.editor-class').should('contain.text', 'other words')
@@ -70,8 +71,9 @@ describe('Yearly assessment form tests', () => {
 
   it("Opening another form and saving to it doesn't affect yearly assesment data", () => {
     cy.get('[data-cy=yearSelector]').contains(defaultYears[1])
-    cy.get('[data-cy=textarea-recruitment_influence]').find('.editor-class').click()
-    cy.get('[data-cy=textarea-recruitment_influence]').find('[contenteditable]').type('new words').wait(2000)
+    cy.typeInEditor('[data-cy=textarea-recruitment_influence]', 'new words')
+    // cy.get('[data-cy=textarea-recruitment_influence]').find('.editor-class').click()
+    // cy.get('[data-cy=textarea-recruitment_influence]').find('[contenteditable]').type('new words').wait(2000)
     cy.reload()
     cy.visit('/')
 
@@ -88,8 +90,9 @@ describe('Yearly assessment form tests', () => {
     // write to other form
     cy.visit('/evaluation/form/4/KH50_005')
     cy.wait(1000)
-    cy.get('[data-cy=textarea-degree_flow]').find('.editor-class').click().wait(500)
-    cy.get('[data-cy=textarea-degree_flow]').find('[contenteditable]').wait(500).type('evaluation words').wait(2000)
+    cy.typeInEditor('[data-cy=textarea-degree_flow]', 'evaluation words')
+    // cy.get('[data-cy=textarea-degree_flow]').find('.editor-class').click().wait(500)
+    // cy.get('[data-cy=textarea-degree_flow]').find('[contenteditable]').wait(500).type('evaluation words').wait(2000)
     cy.reload()
 
     // check yearly assessment form
@@ -116,8 +119,9 @@ describe('Yearly assessment form tests', () => {
     // write to yearly form
     cy.visit(`/form/${testProgrammeCode}`)
     cy.get('[data-cy=yearSelector]').contains(defaultYears[1])
-    cy.get('[data-cy=textarea-employability]').find('.editor-class').click().wait(500)
-    cy.get('[data-cy=textarea-employability]').find('[contenteditable]').wait(1000).type('new words').wait(2000)
+    cy.typeInEditor('[data-cy=textarea-employability]', 'new words')
+    // cy.get('[data-cy=textarea-employability]').find('.editor-class').click().wait(500)
+    // cy.get('[data-cy=textarea-employability]').find('[contenteditable]').wait(1000).type('new words').wait(2000)
     cy.reload()
     cy.visit('/')
 
