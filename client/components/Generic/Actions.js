5931e379fd4ca9b8c0183257c066ad82eafbacef
Laura Sinikallio <46318664+sinikala@users.noreply.github.com>
2023-04-27 15:51:39 +0300

    Improvement actions are saved and fetched properly

--- client/components/Generic/Actions.js
+++ client/components/Generic/Actions.js
@@ -1,47 +1,76 @@
 import React, { useState } from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Button, Confirm, Divider, Input, Label, Icon } from 'semantic-ui-react'
 import { useTranslation } from 'react-i18next'
+import { updateFormField } from 'Utilities/redux/formReducer'
 import { colors } from 'Utilities/common'
-import SimpleTextarea from './SimpleTextarea'
-// import Textarea from './Textarea'
 import './Generic.scss'
 
-const ActionElement = ({ t, id, form, viewOnly, index }) => {
+const ActionElement = ({ id, form, viewOnly, index }) => {
+  const { t } = useTranslation()
+  const dispatch = useDispatch()
+
+  const fieldName = `${id}-${index}-text`
+
+  const actionPoint = useSelector(({ form }) => form.data[fieldName] || { title: '', actions: '' })
+  const { title, actions } = actionPoint
+
+  const handleChange = ({ target }) => {
+    const { id, value } = target
+    // console.log(target.id, target.value)
+    const updated = { ...actionPoint, [id]: value }
+    // console.log(updated)
+    dispatch(updateFormField(fieldName, updated, form))
+  }
+
   return (
     <div key={index}>
       <div style={{ paddingLeft: '2em' }}>
         <Label color="red" ribbon>
           {t('formView:developmentArea')}
         </Label>
-        <Input style={{ width: '50%' }} placeholder={t('formView:developmentArea')} disabled={viewOnly} />
+        <Input
+          id="title"
+          style={{ width: '50%' }}
+          placeholder={t('formView:developmentArea')}
+          onChange={handleChange}
+          disabled={viewOnly}
+          value={title || ''}
+        />
       </div>
-      <SimpleTextarea label={t('formView:actions')} id={`${id}_${index}`} viewOnly={viewOnly} form={form} />
+      <div className="form-textarea">
+        <label>{t('formView:actions')}</label>
+        {viewOnly ? <>{actions}</> : <textarea id="actions" value={actions || ''} onChange={handleChange} />}
+      </div>
     </div>
   )
 }
 
 const Actions = ({ id, label, description, form, required, extrainfo }) => {
   const { t } = useTranslation()
-  // const formData = useSelector(state => state.form.data)
+  const dispatch = useDispatch()
+  const formData = useSelector(state => state.form.data)
   const viewOnly = useSelector(({ form }) => form.viewOnly)
   const [confirm, setConfirm] = useState(false)
-  const [actions, setActions] = useState([{ id: 1, area_title: '', action_text: '' }])
-  const actionCount = actions.length || 0
+
+  const actionsList = Object.keys(formData).filter(questionId => questionId.includes(id)) || []
+  const actionsCount = actionsList.length
+
+  // console.log(actionsList)
 
   const handleAdd = () => {
-    setActions([...actions, { id: actionCount + 1, area_title: '', action_text: '' }])
+    dispatch(updateFormField(`${id}-${actionsCount + 1}-text`, { title: '', actions: '' }, form))
   }
 
   const handleRemove = () => {
-    const newList = actions.reduce((all, current) => {
-      if (current?.id !== actionCount) {
-        return [...all, current]
-      }
-      return all
-    }, [])
-    setActions(newList)
-    setConfirm(false)
+    // const newList = actions.reduce((all, current) => {
+    //   if (current?.id !== actionCount) {
+    //     return [...all, current]
+    //   }
+    //   return all
+    // }, [])
+    // setActions(newList)
+    // setConfirm(false)
   }
 
   return (
@@ -74,29 +103,17 @@ const Actions = ({ id, label, description, form, required, extrainfo }) => {
         {description}
         <p className="form-question-extrainfo">{extrainfo}</p>
       </div>
-      {actions.map((action, index) => {
-        return (
-          <ActionElement
-            key={action.id}
-            t={t}
-            id={id}
-            form={form}
-            viewOnly={viewOnly}
-            action={action}
-            index={index}
-            actions={actions}
-            setActions={setActions}
-          />
-        )
+      {actionsList.map((action, index) => {
+        return <ActionElement key={`action-${index + 1}`} id={id} form={form} viewOnly={viewOnly} index={index + 1} />
       })}
       <div style={{ display: 'flex' }}>
-        {actionCount < 5 && !viewOnly && (
+        {actionsCount < 5 && !viewOnly && (
           <Button icon basic labelPosition="left" color="blue" onClick={handleAdd}>
             <Icon name="add" />
             {t('formView:addDevelopmentArea')}
           </Button>
         )}
-        {actionCount > 0 && !viewOnly && (
+        {actionsCount > 0 && !viewOnly && (
           <div>
             <Button icon basic labelPosition="left" color="red" onClick={() => setConfirm(true)}>
               <Icon name="trash alternate" />
