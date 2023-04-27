b11ecbb786672a60dfd89e27bbdbd9b9270e5834
Laura Sinikallio <46318664+sinikala@users.noreply.github.com>
2023-04-27 16:51:11 +0300

    Finetune actions element

---
+++ client/components/Generic/ActionElement.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Input, Label } from 'semantic-ui-react'
+import { useTranslation } from 'react-i18next'
+import { updateFormField } from 'Utilities/redux/formReducer'
+import './Generic.scss'
+
+const ActionElement = ({ id, form, viewOnly, index }) => {
+  const { t } = useTranslation()
+  const dispatch = useDispatch()
+
+  const fieldName = `${id}-${index}-text`
+  const actionPoint = useSelector(({ form }) => form.data[fieldName] || { title: '', actions: '' })
+  const { title, actions } = actionPoint
+
+  const handleChange = ({ target }) => {
+    const { id, value } = target
+    const updated = { ...actionPoint, [id]: value }
+    dispatch(updateFormField(fieldName, updated, form))
+  }
+
+  return (
+    <div key={index}>
+      <div style={{ paddingLeft: '2em' }}>
+        <Label color="red" ribbon>
+          {t('formView:developmentArea')}
+        </Label>
+        <Input
+          id="title"
+          style={{ width: '50%' }}
+          placeholder={t('formView:developmentArea')}
+          onChange={handleChange}
+          disabled={viewOnly}
+          value={title || ''}
+        />
+      </div>
+      <div className="form-textarea">
+        <label>{t('formView:actions')}</label>
+        {viewOnly ? <>{actions}</> : <textarea id="actions" value={actions || ''} onChange={handleChange} />}
+      </div>
+    </div>
+  )
+}
+
+export default ActionElement
--- client/components/Generic/Actions.js
+++ client/components/Generic/Actions.js
@@ -1,78 +1,32 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Button, Confirm, Divider, Input, Label, Icon } from 'semantic-ui-react'
+import { Button, Divider, Icon } from 'semantic-ui-react'
 import { useTranslation } from 'react-i18next'
 import { updateFormField } from 'Utilities/redux/formReducer'
 import { colors } from 'Utilities/common'
+import ActionElement from './ActionElement'
 import './Generic.scss'
-
-const ActionElement = ({ id, form, viewOnly, index }) => {
-  const { t } = useTranslation()
-  const dispatch = useDispatch()
-
-  const fieldName = `${id}-${index}-text`
-
-  const actionPoint = useSelector(({ form }) => form.data[fieldName] || { title: '', actions: '' })
-  const { title, actions } = actionPoint
-
-  const handleChange = ({ target }) => {
-    const { id, value } = target
-    // console.log(target.id, target.value)
-    const updated = { ...actionPoint, [id]: value }
-    // console.log(updated)
-    dispatch(updateFormField(fieldName, updated, form))
-  }
-
-  return (
-    <div key={index}>
-      <div style={{ paddingLeft: '2em' }}>
-        <Label color="red" ribbon>
-          {t('formView:developmentArea')}
-        </Label>
-        <Input
-          id="title"
-          style={{ width: '50%' }}
-          placeholder={t('formView:developmentArea')}
-          onChange={handleChange}
-          disabled={viewOnly}
-          value={title || ''}
-        />
-      </div>
-      <div className="form-textarea">
-        <label>{t('formView:actions')}</label>
-        {viewOnly ? <>{actions}</> : <textarea id="actions" value={actions || ''} onChange={handleChange} />}
-      </div>
-    </div>
-  )
-}
 
 const Actions = ({ id, label, description, form, required, extrainfo }) => {
   const { t } = useTranslation()
   const dispatch = useDispatch()
   const formData = useSelector(state => state.form.data)
   const viewOnly = useSelector(({ form }) => form.viewOnly)
-  const [confirm, setConfirm] = useState(false)
 
   const actionsList = Object.keys(formData).filter(questionId => questionId.includes(id)) || []
   const actionsCount = actionsList.length
-
-  // console.log(actionsList)
 
   const handleAdd = () => {
     dispatch(updateFormField(`${id}-${actionsCount + 1}-text`, { title: '', actions: '' }, form))
   }
 
-  const handleRemove = () => {
-    // const newList = actions.reduce((all, current) => {
-    //   if (current?.id !== actionCount) {
-    //     return [...all, current]
-    //   }
-    //   return all
-    // }, [])
-    // setActions(newList)
-    // setConfirm(false)
+  const previousHasContent = () => {
+    if (!formData[`${id}-${actionsCount}-text`]) {
+      return false
+    }
+    const latest = formData[`${id}-${actionsCount}-text`]
+    return latest.title.length > 0 || latest.actions.length > 0
   }
-
   return (
     <div
       className="form-entity-area"
@@ -103,24 +57,19 @@ const Actions = ({ id, label, description, form, required, extrainfo }) => {
         {description}
         <p className="form-question-extrainfo">{extrainfo}</p>
       </div>
-      {actionsList.map((action, index) => {
-        return <ActionElement key={`action-${index + 1}`} id={id} form={form} viewOnly={viewOnly} index={index + 1} />
-      })}
+      {actionsList.length === 0 ? (
+        <ActionElement key="action-1" id={id} form={form} viewOnly={viewOnly} index={1} />
+      ) : (
+        actionsList.map((action, index) => {
+          return <ActionElement key={`action-${index + 1}`} id={id} form={form} viewOnly={viewOnly} index={index + 1} />
+        })
+      )}
       <div style={{ display: 'flex' }}>
         {actionsCount < 5 && !viewOnly && (
-          <Button icon basic labelPosition="left" color="blue" onClick={handleAdd}>
+          <Button icon basic labelPosition="left" color="blue" onClick={handleAdd} disabled={!previousHasContent()}>
             <Icon name="add" />
             {t('formView:addDevelopmentArea')}
           </Button>
-        )}
-        {actionsCount > 0 && !viewOnly && (
-          <div>
-            <Button icon basic labelPosition="left" color="red" onClick={() => setConfirm(true)}>
-              <Icon name="trash alternate" />
-              {t('formView:removeDevelopmentArea')}
-            </Button>
-            <Confirm open={confirm} onCancel={() => setConfirm(false)} onConfirm={handleRemove} />
-          </div>
         )}
       </div>
     </div>
