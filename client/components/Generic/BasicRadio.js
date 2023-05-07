21a88b36e7147e73519eb954882a86c1f7851786
iPegii <51372604+iPegii@users.noreply.github.com>
2023-05-07 21:40:55 +0300

    fix basicRadio but number-scale

--- client/components/Generic/BasicRadio.js
+++ client/components/Generic/BasicRadio.js
@@ -12,8 +12,9 @@ const BasicRadio = ({ id, radioButtonLabels, direction, handleClick, viewOnly, t
   }
 
   const isChecked = value => {
-    return type === 'basic' ? checked.firstValue === value : checked.secondValue === value
+    return type === 'basic' ? checked === value : checked.secondValue === value
   }
+
   return (
     <div>
       {radioButtonLabels ? (
