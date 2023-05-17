cfd1d5485724e8a6de561250e54f2595d74ac9a4
Laura Sinikallio <46318664+sinikala@users.noreply.github.com>
2023-05-17 10:33:41 +0300

    Refactor the remaining PDF print functions to use react-to-print

--- client/components/ReportPage/ColorAnswers.js
+++ client/components/ReportPage/ColorAnswers.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import { useSelector } from 'react-redux'
 import { Grid, Radio } from 'semantic-ui-react'
 import { useTranslation } from 'react-i18next'
@@ -11,6 +11,7 @@ import PieChart from './PieChart'
 const ColorAnswers = ({ year, allAnswers, questionsList, chosenProgrammes, setActiveTab, setShowing }) => {
   const { t } = useTranslation()
   const [showEmpty, setShowEmpty] = useState(true)
+  const componentRef = useRef()
   const questions = useSelector(({ filters }) => filters.questions)
 
   if (chosenProgrammes.length < 1 || allAnswers.size < 1) {
@@ -25,12 +26,12 @@ const ColorAnswers = ({ year, allAnswers, questionsList, chosenProgrammes, setAc
   }
 
   return (
-    <div className="tab-pane">
+    <div className="tab-pane" ref={componentRef}>
       <Grid className="header">
         <Grid.Row className="noprint">
           <Grid.Column floated="right">
             <div className="side-note-large">
-              <PDFDownload />
+              <PDFDownload componentRef={componentRef} />
             </div>
             <p className="report side-note-small">{t('report:pdfNotification')}</p>
           </Grid.Column>
