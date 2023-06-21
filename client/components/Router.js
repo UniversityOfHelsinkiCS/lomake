59eea83636250cc16971bec296292b7fb18a47f4
iPegii <51372604+iPegii@users.noreply.github.com>
2023-06-21 12:14:32 +0300

    Change url from "/degree-reform-individual/form" to "/individual"

--- client/components/Router.js
+++ client/components/Router.js
@@ -38,7 +38,7 @@ export default () => (
         path="/degree-reform/form/:room"
         render={props => <DegreeReformFormView room={props.match.params.room} />}
       />
-      <Route exact path="/degree-reform-individual/form" component={DegreeReformIndividualForm} />
+      <Route exact path="/individual" component={DegreeReformIndividualForm} />
       <Route
         exact
         path="/evaluation/form/:form/:room"
