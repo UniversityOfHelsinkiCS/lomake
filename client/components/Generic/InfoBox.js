304857ae5dcca40d38488d1d6abbccc344844dd9
Matti Luukkainen <mluukkai@iki.fi>
2023-08-29 23:04:30 +0300

    study path images en and se

--- client/components/Generic/InfoBox.js
+++ client/components/Generic/InfoBox.js
@@ -1,14 +1,25 @@
 import React, { useEffect, useState } from 'react'
 import { Divider, Button } from 'semantic-ui-react'
 import { useTranslation } from 'react-i18next'
+import { useSelector } from 'react-redux'
 import ReactMarkdown from 'react-markdown'
-import studyPath from '../../assets/degreeReform/study_path.png'
+import studyPathFi from '../../assets/degreeReform/study_path.png'
+import studyPathEn from '../../assets/degreeReform/study_path_en.png'
+import studyPathSe from '../../assets/degreeReform/study_path_se.png'
 import './Generic.scss'
+
+const getImageFor = lang => {
+  if (lang === 'fi') return studyPathFi
+  if (lang === 'en') return studyPathEn
+  return studyPathSe
+}
 
 const InfoBox = ({ id, label, description, extrainfo, image }) => {
   const { t } = useTranslation()
   const [accordion, setAccordion] = useState({ open: false, fetched: null, lines: 0 })
   let lines = 0
+  const lang = useSelector(state => state.language)
+
   useEffect(() => {
     lines = description.split(/\r\n|\r|\n/).length
     lines += label ? label.split(/\r\n|\r|\n/).length : 0
@@ -59,7 +70,7 @@ const InfoBox = ({ id, label, description, extrainfo, image }) => {
             {label}
           </p>
         )}
-        {image ? <img src={studyPath} alt="three-step" style={{ maxWidth: '100%', height: 'auto' }} /> : null}
+        {image ? <img src={getImageFor(lang)} alt="three-step" style={{ maxWidth: '100%', height: 'auto' }} /> : null}
       </div>
       {accordion.fetched && accordion.lines > 4 ? (
         <Button
