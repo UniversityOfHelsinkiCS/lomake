46c7f2893bf6156058d8ba30c1bde8f3cf59c7e8
Veikkosuhonen <vesuvesuh@gmail.com>
2023-09-04 16:11:07 +0300

    Delete token migrations

--- migrations/202002251524-add-valid-column-to-tokens.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.addColumn('tokens', 'valid', Sequelize.BOOLEAN)
-  },
-
-  down: queryInterface => {
-    return queryInterface.removeColumn('tokens', 'valid')
-  },
-}
