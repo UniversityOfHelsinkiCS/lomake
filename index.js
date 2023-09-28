293d89aaf3c385ad7e7da6d57523b316b421702d
iPegii <51372604+iPegii@users.noreply.github.com>
2023-09-28 13:03:22 +0300

    update socket.io depedencies

--- index.js
+++ index.js
@@ -15,53 +15,55 @@ const { startBackupJob } = require('@root/server/scripts/backupAnswers')
 const { startDeadlineWatcher } = require('@root/server/scripts/deadlineWatcher')
 const { seed } = require('@root/server/scripts/seed')
 const { getUserList } = require('@root/server/scripts/getUserList')
-const { createTempAnswers } = require('@root/server/scripts/createTempAnswers');
+const { createTempAnswers } = require('@root/server/scripts/createTempAnswers')
 
-initializeDatabaseConnection().then(() => {
-  // Scripts that can be ran manually
-  if (process.argv[2]) {
-    switch (process.argv[2]) {
-      case 'seed':
-        seed()
-        return
-      case 'getUserList':
-        getUserList()
-        return
-      case 'generateMissingTempAnswers':
-        createTempAnswers()
-        return
-      default:
-        return
+initializeDatabaseConnection()
+  .then(() => {
+    // Scripts that can be ran manually
+    if (process.argv[2]) {
+      switch (process.argv[2]) {
+        case 'seed':
+          seed()
+          return
+        case 'getUserList':
+          getUserList()
+          return
+        case 'generateMissingTempAnswers':
+          createTempAnswers()
+          return
+        default:
+          return
+      }
     }
-  }
 
-  const app = express()
-  const server = http.Server(app)
-  const io = require('socket.io')(server)
-  io.on('connection', socket => {
-    socket.on('update_field', room => require('@util/websocketHandlers').updateField(socket, room, io))
-    socket.on('join', (room, form) => require('@util/websocketHandlers').joinRoom(socket, room, form, io))
-    socket.on('leave', room => require('@util/websocketHandlers').leaveRoom(socket, room))
-    socket.on('get_lock', room => require('@util/websocketHandlers').getLock(socket, room, io))
-  })  
+    const app = express()
+    const server = http.Server(app)
+    const io = require('socket.io')(server)
+    io.on('connection', socket => {
+      socket.on('update_field', room => require('@util/websocketHandlers').updateField(socket, room, io))
+      socket.on('join', (room, form) => require('@util/websocketHandlers').joinRoom(socket, room, form, io))
+      socket.on('leave', room => require('@util/websocketHandlers').leaveRoom(socket, room))
+      socket.on('get_lock', room => require('@util/websocketHandlers').getLock(socket, room, io))
+    })
 
-  // Require is here so we can delete it from cache when files change (*)
-  app.use('/api', (req, res, next) => require('@root/server')(req, res, next)) // eslint-disable-line
+    // Require is here so we can delete it from cache when files change (*)
+    app.use('/api', (req, res, next) => require('@root/server')(req, res, next)) // eslint-disable-line
 
-  if (inProduction || inStaging) {
-    const DIST_PATH = path.resolve(__dirname, './build')
-    const INDEX_PATH = path.resolve(DIST_PATH, 'index.html')
+    if (inProduction || inStaging) {
+      const DIST_PATH = path.resolve(__dirname, './build')
+      const INDEX_PATH = path.resolve(DIST_PATH, 'index.html')
 
-    app.use(express.static(DIST_PATH))
-    app.get('*', (req, res) => res.sendFile(INDEX_PATH))
-  }
+      app.use(express.static(DIST_PATH))
+      app.get('*', (req, res) => res.sendFile(INDEX_PATH))
+    }
 
-  server.listen(PORT, () => {
-    logger.info(`Started on port ${PORT}`)
-    startBackupJob()
-    startDeadlineWatcher()
+    server.listen(PORT, () => {
+      logger.info(`Started on port ${PORT}`)
+      startBackupJob()
+      startDeadlineWatcher()
+    })
   })
-}).catch(e => {
-  process.exitCode = 1
-  logger.error(e)
-})
+  .catch(e => {
+    process.exitCode = 1
+    logger.error(e)
+  })
