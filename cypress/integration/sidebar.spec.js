d454bbeb202cd21867b94d994a0b0987e10b260b
Veikkosuhonen <vesuvesuh@gmail.com>
2023-10-05 12:31:41 +0300

    Delete very flaky test

--- cypress/integration/sidebar.spec.js
+++ cypress/integration/sidebar.spec.js
@@ -14,26 +14,11 @@ describe('Sidebar tests', () => {
     cy.get(`[data-cy=colortable-link-to-${testProgrammeCode}]`).click()
   })
 
-  // Skip for now, figure out later why this does not work in CI-pipeline when it works locally
   it('Answer length of 1 is OK', () => {
     cy.typeInEditor('review_of_last_years_situation_report', 'A')
 
     cy.get('[data-cy=review_of_last_years_situation_report-EMPTY]')
     cy.get('[data-cy=color-negative-review_of_last_years_situation_report]').click()
     cy.get('[data-cy=review_of_last_years_situation_report-OK]')
-  })
-
-  // Cypress and react-editor do not currently work together, when trying to copy text
-  // Copying to the editor however actually works
-  it('Answer length 1000 of is OK', () => {
-    cy.typeInEditor('review_of_last_years_situation_report', 'A'.repeat(1000), 0)
-
-    cy.get('[data-cy=review_of_last_years_situation_report-EMPTY]')
-    cy.get('[data-cy=color-positive-review_of_last_years_situation_report]').click()
-    cy.get('[data-cy=review_of_last_years_situation_report-OK]')
-
-    cy.getEditorInputLength('[data-cy=textarea-review_of_last_years_situation_report]').then(res =>
-      expect(res).to.be.eq(1000)
-    )
   })
 })
