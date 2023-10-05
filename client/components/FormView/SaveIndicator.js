fe94b57d6fed4b3a49e3b55e2e589669653e8cde
Veikkosuhonen <vesuvesuh@gmail.com>
2023-10-05 12:32:06 +0300

    Delete save error functionality

--- client/components/FormView/SaveIndicator.js
+++ client/components/FormView/SaveIndicator.js
@@ -1,105 +1,19 @@
-/* eslint-disable no-console */
-import React, { useState, useEffect } from 'react'
-import { Button, Message, Icon } from 'semantic-ui-react'
-import { useDispatch, useSelector } from 'react-redux'
+import React from 'react'
+import { Button } from 'semantic-ui-react'
+import { useSelector } from 'react-redux'
 import { useTranslation } from 'react-i18next'
-import { colors } from 'Utilities/common'
-import { setViewOnly } from 'Utilities/redux/formReducer'
-import { Sentry } from 'Utilities/sentry'
 
 // eslint-disable-next-line react/function-component-definition
 export default function SaveIndicator() {
   const { t } = useTranslation()
-  const [saving, setSaving] = useState(false)
-  const [saveError, setSaveError] = useState(false)
-  const [timeoutId, setTimeoutId] = useState(undefined)
-
-  const isIndividualForm = window.location.href.includes('/individual')
 
   const lastSaveSuccess = useSelector(state => state.form.lastSaveSuccess)
-  const lastSaveAttempt = useSelector(state => state.form.lastSaveAttempt)
-
-  const formError = useSelector(state => state.form.error)
 
   const lang = useSelector(state => state.language)
   const viewOnly = useSelector(state => state.form.viewOnly)
-  const dispatch = useDispatch()
 
-  // eslint-disable-next-line no-unused-vars
-  const errorHandler = () => {
-    if (isIndividualForm) return
-    if (viewOnly) return
-    setSaveError(true)
-    setTimeoutId(undefined)
-    dispatch(setViewOnly(true))
-    Sentry.captureMessage('Form save failed')
-  }
+  if (viewOnly) return null
 
-  useEffect(() => {
-    if (formError) {
-      setSaveError(true)
-    }
-  }, [formError])
-
-  useEffect(() => {
-    if (isIndividualForm) return undefined
-    setSaving(true)
-    if (timeoutId || viewOnly) return undefined
-
-    console.log('STARTING TIMER')
-    const temp = setTimeout(() => {
-      // DO NOT DO THIS ANYMORE errorHandler()
-    }, 10000)
-    setTimeoutId(temp)
-
-    return () => {
-      clearTimeout(temp)
-    }
-  }, [lastSaveAttempt, viewOnly])
-
-  useEffect(() => {
-    if (!isIndividualForm && saving) {
-      console.log('SAVE SUCCESS')
-      setSaving(false)
-      clearTimeout(timeoutId)
-      setTimeoutId(undefined)
-    }
-  }, [lastSaveSuccess])
-
-  if (viewOnly && !saveError) return null
-
-  if (saveError) {
-    return (
-      <>
-        <Message
-          negative
-          icon={<Icon color={colors.red} name="exclamation" />}
-          style={{
-            position: 'fixed',
-            left: '50%',
-            transform: 'translate(-50%)',
-            top: '5em',
-            zIndex: 100,
-            width: '75%',
-            maxWidth: '50em',
-          }}
-          header={`${t('formView:saveFailed')}`}
-          content={
-            <div style={{ paddingTop: '1em' }}>
-              <span>{t('formView:saveFailedInstructions')}</span>
-              <Button
-                style={{ marginTop: '2em', float: 'right' }}
-                color="blue"
-                onClick={() => window.location.reload()}
-              >
-                {t('formView:reload')}
-              </Button>
-            </div>
-          }
-        />
-      </>
-    )
-  }
   return (
     <>
       <Button
