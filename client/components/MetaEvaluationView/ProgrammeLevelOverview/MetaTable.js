107e2086ee0839b29ebc6ccb6646996d4112e3bb
tfhuhtal <tuomas.huhtala@icloud.com>
2024-06-06 16:28:03 +0300

    sorting added

--- client/components/MetaEvaluationView/ProgrammeLevelOverview/MetaTable.js
+++ client/components/MetaEvaluationView/ProgrammeLevelOverview/MetaTable.js
@@ -1,15 +1,52 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect, useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
+import { Loader, Icon } from 'semantic-ui-react'
+import { getTempAnswersByFormAndYear } from 'Utilities/redux/tempAnswersReducer'
+import { sortedItems } from 'Utilities/common'
 
-const MetaTable = ({ programmes, questions, answers }) => {
+const MetaTable = ({ programmes, questions }) => {
   const lang = useSelector(state => state.language)
+  const { t } = useTranslation()
+  const dispatch = useDispatch()
+  const answers = useSelector(state => state.tempAnswers)
+  const [sorter, setSorter] = useState('name')
+  const [reverse, setReverse] = useState(false)
 
+  const form = 7
+
+  useEffect(() => {
+    dispatch(getTempAnswersByFormAndYear(form, 2024))
+  }, [dispatch])
+
+  const sortedProgrammes = sortedItems(programmes, sorter, lang)
+  if (reverse) sortedProgrammes.reverse()
+
+  const sort = sortValue => {
+    setSorter(sortValue)
+    setReverse(!reverse)
+  }
+
+  if (answers.pending || !answers.data) {
+    return <Loader active inline="centered" />
+  }
   return (
     <table border="1">
       <thead>
         <tr>
-          <th>Programme / Question</th>
+          <th>
+            <div onClick={() => sort('name')}>
+              {t('programmeHeader')}
+              <Icon name="sort" />
+            </div>
+          </th>
+          <th>
+            <div onClick={() => sort('key')}>
+              {t('code')}
+              <Icon name="sort" />
+            </div>
+          </th>
           {questions.map((question, index) => (
             <th key={question.id}>{index + 1}</th>
           ))}
@@ -20,6 +57,9 @@ const MetaTable = ({ programmes, questions, answers }) => {
           <tr key={programme.id}>
             <td>
               <Link to={`/meta-evaluation/form/${programme.key}`}>{programme.name[lang]}</Link>
+            </td>
+            <td>
+              <Link to={`/meta-evaluation/form/${programme.key}`}>{programme.key}</Link>
             </td>
             {questions.map(question => {
               const programmeAnswers = answers.data.find(answer => answer.programme === programme.key)
--- client/components/MetaEvaluationView/ProgrammeLevelOverview/index.js
+++ client/components/MetaEvaluationView/ProgrammeLevelOverview/index.js
@@ -1,14 +1,10 @@
 import React, { useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
 import { useTranslation } from 'react-i18next'
 import { useVisibleOverviewProgrammes } from 'Utilities/overview'
-import { getTempAnswersByFormAndYear } from 'Utilities/redux/tempAnswersReducer'
-import { Loader } from 'semantic-ui-react'
 
 import { metareviewQuestions as questions } from '@root/client/questionData/index'
 import MetaTable from './MetaTable'
-
-const form = 7
 
 const ProgrammeLevelOverview = () => {
   const { t } = useTranslation()
@@ -16,27 +12,17 @@ const ProgrammeLevelOverview = () => {
   const lang = useSelector(state => state.language)
   const currentUser = useSelector(state => state.currentUser)
   const programmes = useSelector(({ studyProgrammes }) => studyProgrammes.data)
-  const dispatch = useDispatch()
-  const answers = useSelector(state => state.tempAnswers)
 
   useEffect(() => {
     document.title = `${t('overview')}`
   }, [lang])
 
-  useEffect(() => {
-    dispatch(getTempAnswersByFormAndYear(form, 2024))
-  }, [dispatch])
-
   const usersProgrammes = useVisibleOverviewProgrammes({ currentUser, programmes, showAllProgrammes })
-
-  if (answers.pending || !answers.data) {
-    return <Loader active inline="centered" />
-  }
 
   return (
     <div>
       <h1>Programme Level Overview</h1>
-      <MetaTable programmes={usersProgrammes} questions={questions} answers={answers} />
+      <MetaTable programmes={usersProgrammes} questions={questions} />
     </div>
   )
 }
