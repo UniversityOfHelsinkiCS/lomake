e28eb5c304f28fa9b18bb08bd0d8d5334ef81e1c
tfhuhtal <tuomas.huhtala@icloud.com>
2024-06-07 15:38:41 +0300

    fixed form ui

--- client/components/MetaEvaluationView/ProgrammeForm/index.js
+++ client/components/MetaEvaluationView/ProgrammeForm/index.js
@@ -1,14 +1,17 @@
 import React, { useEffect } from 'react'
 import MetaEntity from 'Components/Generic/MetaEntity'
 import { useSelector, useDispatch } from 'react-redux'
-import { Redirect } from 'react-router'
+import { Redirect, useHistory } from 'react-router'
 import { getProgramme } from 'Utilities/redux/studyProgrammesReducer'
 import { useTranslation } from 'react-i18next'
-import { Loader } from 'semantic-ui-react'
+import { Loader, Button } from 'semantic-ui-react'
 import { wsJoinRoom, wsLeaveRoom } from 'Utilities/redux/websocketReducer'
 import { setViewOnly, getSingleProgrammesAnswers } from 'Utilities/redux/formReducer'
-import { getFormViewRights } from 'Utilities/common'
+import { getFormViewRights, colors } from 'Utilities/common'
 import { isAdmin } from '@root/config/common'
+import StatusMessage from 'Components/FormView/StatusMessage'
+import calendarImage from 'Assets/calendar.jpg'
+import SaveIndicator from 'Components/FormView/SaveIndicator'
 
 import { metareviewQuestions as questions } from '../../../questionData'
 // tämä on samanlainen kuin Evaluationiew/EvaluationFormView/index.js
@@ -22,6 +25,7 @@ const ProgrammeLevelForm = ({ room }) => {
   const user = useSelector(state => state.currentUser.data)
   const currentRoom = useSelector(state => state.room)
   const programme = useSelector(state => state.studyProgrammes.singleProgram)
+  const history = useHistory()
   const year = 2024
   const { draftYear, nextDeadline } = useSelector(state => state.deadlines)
   const formDeadline = nextDeadline ? nextDeadline.find(d => d.form === form) : null
@@ -90,11 +94,26 @@ const ProgrammeLevelForm = ({ room }) => {
   }
 
   return (
-    <div>
-      <h1>{programme.name[lang]}</h1>
-      {questions.map(question => (
-        <div key={question.id}>{partMap(question)}</div>
-      ))}
+    <div className="form-container">
+      <div className="the-form">
+        <div className="hide-in-print-mode">
+          <SaveIndicator />
+          <div style={{ marginBottom: '2em' }}>
+            <Button onClick={() => history.push('/meta-evaluation')} icon="arrow left" />
+          </div>
+          <img alt="form-header-calendar" className="img-responsive" src={calendarImage} />
+        </div>{' '}
+        <h1 style={{ color: colors.blue }}>{programme.name[lang]}</h1>
+        <h3 style={{ marginTop: '0' }} data-cy="formview-title">
+          {t('evaluation')} {year}
+        </h3>
+        <div className="hide-in-print-mode">
+          <StatusMessage form={form} writeAccess={writeAccess} />
+        </div>
+        {questions.map(question => (
+          <div key={question.id}>{partMap(question)}</div>
+        ))}
+      </div>
     </div>
   )
 }
