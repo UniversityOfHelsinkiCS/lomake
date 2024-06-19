c396e4eb4c0286f09ed9e90a1f412c4895d47392
tfhuhtal <tuomas.huhtala@icloud.com>
2024-06-19 16:52:17 +0300

    new answers page

--- client/components/MetaEvaluationView/ProgrammeLevelAnswers/index.js
+++ client/components/MetaEvaluationView/ProgrammeLevelAnswers/index.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { useTranslation } from 'react-i18next'
-import { useHistory } from 'react-router-dom'
-import { getTempAnswersByFormAndYear } from 'Utilities/redux/tempAnswersReducer'
-import { Button } from 'semantic-ui-react'
+import * as answer from '@models/answer'
 import { metareviewQuestions as questionData } from '@root/client/questionData/index'
+import { getTempAnswersByFormAndYear } from 'Utilities/redux/tempAnswersReducer'
+import React, { useEffect } from 'react'
+import { useTranslation } from 'react-i18next'
+import { useDispatch, useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+import { Button } from 'semantic-ui-react'
 
 const ProgrammeLevelAnswers = () => {
   const lang = useSelector(state => state.language)
@@ -16,32 +17,38 @@ const ProgrammeLevelAnswers = () => {
   const form = 7
 
   useEffect(() => {
-    document.title = `${t('overview')}`
+    document.title = `${t('evaluation')}`
   }, [lang, t])
 
   useEffect(() => {
     dispatch(getTempAnswersByFormAndYear(form, year))
   }, [dispatch])
 
-  if (!answers.data) return null
+  if (!answers.data || !lang) return null
 
   return (
     <div>
       <div style={{ marginBottom: '2em' }}>
         <Button onClick={() => history.push('/meta-evaluation')} icon="arrow left" />
       </div>
-      {questionData.map(question =>
-        question.parts.map(part => {
-          return (
-            <div key={part.id} style={{ marginBottom: '2em' }}>
-              <h2>
-                {part.index}: {part.label[lang]}
-              </h2>
-            </div>
-          )
-        }),
-      )}
-    </div>
+
+      {questionData.map(question => question.parts.map(action => {
+        const programAnswers = answers.data ? answers.data.find(answer => answer.data[`${action.id}_text`] !== undefined) : null
+        console.log(programAnswers)
+        return (
+          <div>
+            {action.label[lang]}
+            {programAnswers && (
+              <>
+                <div>{programAnswers.programme}</div>
+                <div>{programAnswers.data[`${action.id}_text`]}</div>
+              </>
+            )}
+          </div>
+        )
+      }
+      ))}
+    </div >
   )
 }
 
