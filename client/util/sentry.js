7661a14b9a54e689891d69062f46d52abaac000b
tfhuhtal <tuomas.huhtala@icloud.com>
2024-08-15 10:35:20 +0300

    unfuck sentry sourcemaps

--- client/util/sentry.js
+++ client/util/sentry.js
@@ -1,11 +1,12 @@
 import * as Sentry from '@sentry/browser'
+import { BrowserTracing } from '@sentry/tracing'
 
 const initSentry = () => {
   Sentry.init({
     dsn: 'https://a6ceb1539cbea12e147f5e73189a1d45@toska.cs.helsinki.fi/13',
     environment: process.env.SENTRY_ENVIRONMENT,
     // release: process.env.SENTRY_RELEASE,
-    integrations: [Sentry.browserTracingIntegration()],
+    integrations: [new BrowserTracing()],
     tracesSampleRate: 1.0,
   })
 }
