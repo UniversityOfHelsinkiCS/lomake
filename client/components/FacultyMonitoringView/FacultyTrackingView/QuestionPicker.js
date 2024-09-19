97e135086778539d7d41ec75d34c70fa1eb36406
Venla <venla.e.rantanen@helsinki.fi>
2024-09-19 14:37:19 +0300

    fix dropdown active text bug in question picker

--- client/components/FacultyMonitoringView/FacultyTrackingView/QuestionPicker.js
+++ client/components/FacultyMonitoringView/FacultyTrackingView/QuestionPicker.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Dropdown } from 'semantic-ui-react'
 import { updateFormField } from 'Utilities/redux/formReducer'
@@ -7,36 +7,46 @@ import '../../Generic/Generic.scss'
 const QuestionPicker = ({ label, questionsList, form }) => {
   const dispatch = useDispatch()
   const lang = useSelector(state => state.language)
-  const fieldName = `selectedQuestionIds`
-  const selectedQuestions = useSelector(({ form }) => form.data[fieldName] || [])
   const viewOnly = useSelector(({ form }) => form.viewOnly)
+  const allSelectedQuestions = useSelector(({ form }) => form.data.selectedQuestionIds || [])
+  const [sectionQuestions, setSectionQuestions] = useState([])
 
-  const handleChange = ({ id, value }) => dispatch(updateFormField(id, value, form))
+  useEffect(() => {
+    const questionsInSection = allSelectedQuestions.filter(id => questionsList.some(question => question.id === id))
+    setSectionQuestions(questionsInSection)
+  }, [allSelectedQuestions, questionsList])
 
-  const options = questionsList
-    .map(q => {
-      return { key: `${q.id}`, text: `${q.index}. ${q.label[lang]}`, value: q.id }
-    })
-    .filter(Boolean) // Filter out any undefined options
-
-  const addToList = (_, { value }) => {
-    handleChange({ id: fieldName, value })
+  const updateAllSelectedQuestions = newQuestion => {
+    const otherSectionQuestions = allSelectedQuestions.filter(id => !questionsList.some(question => question.id === id))
+    const selectedQuestions = [...otherSectionQuestions, ...newQuestion]
+    dispatch(updateFormField('selectedQuestionIds', selectedQuestions, form))
   }
 
-  if (viewOnly && (!selectedQuestions || selectedQuestions.length === 0)) return null // Don't render if viewOnly and no selected questions
+  const handleSelectionChange = (_, { value: newQuestion }) => {
+    setSectionQuestions(newQuestion)
+    updateAllSelectedQuestions(newGroupSelection)
+  }
+
+  const dropdownOptions = questionsList.map(question => ({
+    key: question.id,
+    text: `${question.index}. ${question.label[lang]}`,
+    value: question.id,
+  }))
+
+  if (viewOnly && sectionQuestions.length === 0) return null
 
   return (
     <div className="questions-list-container" data-cy="question-picker">
-      <label className={`questions-list-label${selectedQuestions.length === 0 ? '-bolded' : ''}`}>{label}</label>
+      <label className={`questions-list-label${sectionQuestions.length === 0 ? '-bolded' : ''}`}>{label}</label>
       <Dropdown
         className="comparison-questions-list-selector"
         data-cy="questions-list"
         name="questions-list"
         fluid
         placeholder={label}
-        options={options}
-        onChange={addToList}
-        value={selectedQuestions}
+        options={dropdownOptions}
+        onChange={handleSelectionChange}
+        value={sectionQuestions}
         multiple
         selection
       />
