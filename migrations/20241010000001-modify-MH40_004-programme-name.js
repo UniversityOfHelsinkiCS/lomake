a2ee583c4101b846b527107638fb5cca25f1c569
tfhuhtal <tuomas.huhtala@icloud.com>
2024-10-10 10:00:26 +0300

    changing MH40_004 name

---
+++ migrations/20241010000001-modify-MH40_004-programme-name.js
@@ -0,0 +1,10 @@
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.sequelize.query(`UPDATE studyprogrammes SET name='{
+      "fi": "Venäjän, Euraasian ja itäisen Euroopan tutkimuksen maisteriohjelma",
+      "en": "Master''s Programme in Russian, Eurasian and Eastern European Studies",
+      "se": "Magisterprogrammet i ryska, eurasiska och östeuropeiska studier"
+        }' where key='MH40_004'`),
+
+  down: queryInterface => {},
+}
