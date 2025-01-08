530297ef70d37f97fe9b76a97ba01f89fd228462
sutigit <panyanilben@gmail.com>
2025-01-08 13:31:50 +0200

    add comments model

---
+++ server/models/comments.js
@@ -0,0 +1,25 @@
+module.exports = (sequelize, DataTypes) => {
+    const comments = sequelize.define(
+        'comments',
+        {
+            studyprogrammeId: DataTypes.INTEGER,
+            keyMetric: DataTypes.STRING, // avainluku
+            content: DataTypes.JSONB,
+            lastEditedBy: DataTypes.INTEGER, // User id 
+        },
+        {
+            underscored: true,
+            tableName: 'comments',
+        },
+    )
+
+    comments.associate = models => {
+        // sutdyprogramme has a set of comments
+        comments.belongsTo(models.studyprogram, {
+            foreignKey: 'studyprogrammeId',
+            as: 'associatedStudyprogramme',
+        })
+    }
+
+    return comments
+}
