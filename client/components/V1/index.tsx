982655b3ba3a6c5af6186094222cf01177b6aad6
sutigit <panyanilben@gmail.com>
2025-01-23 14:38:25 +0200

    revert changes

--- client/components/V1/index.tsx
+++ client/components/V1/index.tsx
@@ -1,7 +1,5 @@
 import useFetchKeyData from '../../hooks/useFetchKeyData'
 
-import TextFieldComponent from './Generic/TextFieldComponent'
-
 const Page = () => {
   const keyData = useFetchKeyData()
 
@@ -9,13 +7,6 @@ const Page = () => {
     <div>
       <h1>Page</h1>
       <p>{JSON.stringify(keyData)}</p>
-
-      <br />
-      <div>
-        <h1>Testattava teksikenttä</h1>
-        <TextFieldComponent />
-        
-      </div>
     </div>
   )
 }
