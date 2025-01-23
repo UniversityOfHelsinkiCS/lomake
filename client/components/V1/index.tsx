9eb75d75a93c0e516b12d3fd44742e4c203583b2
tfhuhtal <tuomas.huhtala@icloud.com>
2025-01-23 15:46:35 +0200

    fix

--- client/components/V1/index.tsx
+++ client/components/V1/index.tsx
@@ -4,13 +4,9 @@ import DataComponent from './DataComponent'
 import TextFieldComponent from './Generic/TextFieldComponent'
 
 const Page = () => {
-  const keyData = useFetchKeyData()
-
   return (
     <div>
       <h1>Page</h1>
-      <p>{JSON.stringify(keyData)}</p>
-
       <br />
       <div>
         <h1>Testattava teksikenttä</h1>
