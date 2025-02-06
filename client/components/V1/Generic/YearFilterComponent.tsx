9c41eb1e09296a44c81c3d93c2af48bb83b7f9ab
sutigit <panyanilben@gmail.com>
2025-02-06 10:38:12 +0200

    add yearfilter component

--- client/components/V1/Generic/YearFilterComponent.tsx
+++ client/components/V1/Generic/YearFilterComponent.tsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useTranslation } from 'react-i18next'
+import { RootState } from '../../../util/store';
+import { setYear, clearLevelSpecificFilters } from '../../../util/redux/filterReducer'
 
-export default function YearFilterComponent() {
+import {
+  MenuItem,
+  FormControl,
+} from '@mui/material';
+
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+
+
+const YearFilterComponent = () => {
+  const { t } = useTranslation()
+  const dispatch = useDispatch()
+  const selectedYear = useSelector((state: RootState) => state.filters.year)
+  
+  // TODO: How do we handle years?
+  const years = [
+    { key: 0, value: '2025', text: '2025' },
+  ]
+
+  const handleChange = (event: SelectChangeEvent<string[]>) => {
+    // TODO: Consult! What does this do?
+    dispatch(clearLevelSpecificFilters())
+
+    const value = event.target.value as string;
+    dispatch(setYear(value))
+  }
+
   return (
-    <div>YearFilterComponent</div>
-  )
+    <div>
+      <FormControl sx={{ m: 1, width: 350 }}>
+        <Select
+          id="level-filter"
+          value={selectedYear}
+          onChange={handleChange}
+        >
+          {years.map((option) => (
+            <MenuItem key={option.key} value={option.value}>
+              {option.text}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </div>
+  );
 }
+
+export default YearFilterComponent
