742c95f2f2947beb48f01dca3641842bf64e7dd7
tfhuhtal <tuomas.huhtala@icloud.com>
2025-02-11 14:43:29 +0200

    fix staging and prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import react from '@vitejs/plugin-react'
 import { AliasOptions, defineConfig } from 'vite'
-import { inProduction } from './config/common'
 import { sentryVitePlugin } from '@sentry/vite-plugin'
 import path from "path";
 const root = path.resolve(__dirname, './');
 
 export default defineConfig({
   plugins: [react(), sentryVitePlugin()],
-  base: inProduction ? '/tilannekuva' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/tilannekuva' : '/',
   resolve: {
     alias: {
       '@': root,
