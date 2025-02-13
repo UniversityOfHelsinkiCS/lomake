b85ab6e5222733bbe643bef4bc867d7f2f1cac0a
sutigit <panyanilben@gmail.com>
2025-02-13 14:37:51 +0200

    add data modal template component

--- client/components/V1/Generic/ModalTemplateComponent.tsx
+++ client/components/V1/Generic/ModalTemplateComponent.tsx
@@ -1,7 +1,6 @@
-import { useState } from 'react'
+import React from 'react'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
-import Typography from '@mui/material/Typography'
 import Modal from '@mui/material/Modal'
 
 const style = {
@@ -16,28 +15,23 @@ const style = {
   p: 4,
 }
 
-export default function ModalTemplateComponent() {
-  const [open, setOpen] = useState(false)
+export default function ModalTemplateComponent({
+  children,
+  open,
+  setOpen,
+}: {
+  children: React.ReactNode
+  open: boolean
+  setOpen: any
+}) {
   const handleOpen = () => setOpen(true)
   const handleClose = () => setOpen(false)
 
   return (
     <div>
       <Button onClick={handleOpen}>Open modal</Button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
+      <Modal open={open} onClose={handleClose}>
+        <Box sx={style}>{children}</Box>
       </Modal>
     </div>
   )
