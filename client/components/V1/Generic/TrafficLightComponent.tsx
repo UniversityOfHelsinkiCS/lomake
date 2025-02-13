5f024eabadb62d309e7243ad1870d7fe8cdc704c
sutigit <panyanilben@gmail.com>
2025-02-13 14:05:30 +0200

    add trafficlight color tooltip

--- client/components/V1/Generic/TrafficLightComponent.tsx
+++ client/components/V1/Generic/TrafficLightComponent.tsx
@@ -1,38 +1,46 @@
+import { Tooltip } from '@mui/material'
 import { customColors } from '../../../../theme'
+import { useTranslation } from 'react-i18next'
 
 interface TrafficLightProps {
   color: string
   style?: any
 }
 
-const getColorHex = (color: string) => {
-  switch (color) {
-    case 'Vihreä':
-      return customColors.greenLight
-    case 'Keltainen':
-      return customColors.yellowLight
-    case 'Punainen':
-      return customColors.redLight
-    default:
-      return customColors.grayLight
-  }
-}
-
 export const TrafficLight = (props: TrafficLightProps) => {
-  const colorHex = getColorHex(props.color)
+  const { t } = useTranslation()
+
+  const getColorData = (color: string) => {
+    switch (color) {
+      case 'Vihreä':
+        return { hex: customColors.greenLight, color: t('common:green') }
+      case 'Keltainen':
+        return { hex: customColors.yellowLight, color: t('common:yellow') }
+      case 'Punainen':
+        return { hex: customColors.redLight, color: t('common:red') }
+      default:
+        return { hex: customColors.grayLight, color: t('common:gray') }
+    }
+  }
+
+  const colorData = getColorData(props.color)
+  const hex = colorData.hex
+  const text = colorData.color
   const isDisplayed = props.color === 'Tyhjä' ? 'none' : 'block'
   return (
-    <div
-      style={{
-        backgroundColor: colorHex,
-        display: isDisplayed,
-        minWidth: '25px',
-        width: '25px',
-        minHeight: '25px',
-        height: '25px',
-        borderRadius: '50%',
-        ...props.style,
-      }}
-    ></div>
+    <Tooltip title={text} arrow>
+      <div
+        style={{
+          backgroundColor: hex,
+          display: isDisplayed,
+          minWidth: '25px',
+          width: '25px',
+          minHeight: '25px',
+          height: '25px',
+          borderRadius: '50%',
+          ...props.style,
+        }}
+      ></div>
+    </Tooltip>
   )
 }
