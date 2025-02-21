c92e7f76d0f3f3b3fdeefc5274ce63bd170c6b17
sutigit <panyanilben@gmail.com>
2025-02-21 13:29:56 +0200

    refactor local and shared enums

---
+++ shared/lib/enums.ts
@@ -0,0 +1,6 @@
+export enum GroupKey {
+  VETOVOIMAISUUS = 'Vetovoimaisuus',
+  LAPIVIRTAUS = 'Läpivirtaus ja valmistuminen',
+  OPISKELIJAPALAUTE = 'Opiskelijapalaute ja työllistyminen',
+  RESURSSIT = 'Resurssit',
+}
