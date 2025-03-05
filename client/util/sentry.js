5683eae8d81cc287fee07d0810fa344ac5a6b2de
tfhuhtal <tuomas.huhtala@icloud.com>
2025-03-05 15:22:03 +0200

    adding sentry release

--- client/util/sentry.js
+++ client/util/sentry.js
@@ -1,10 +1,12 @@
 import * as Sentry from '@sentry/browser'
 
+const GIT_SHA = process.env.GIT_SHA || ''
+
 const initSentry = () => {
   Sentry.init({
     dsn: 'https://a6ceb1539cbea12e147f5e73189a1d45@toska.cs.helsinki.fi/13',
     environment: process.env.SENTRY_ENVIRONMENT,
-    // release: process.env.SENTRY_RELEASE,
+    release: GIT_SHA,
     integrations: [
       Sentry.breadcrumbsIntegration(),
       Sentry.browserTracingIntegration(),
