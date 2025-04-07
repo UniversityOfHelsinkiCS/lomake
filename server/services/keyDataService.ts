9e220258bb7377aa91c360c57e7cf2d52d1e1de1
tfhuhtal <tuomas.huhtala@icloud.com>
2025-04-07 13:22:06 +0300

    initial excel mods

--- server/services/keyDataService.ts
+++ server/services/keyDataService.ts
@@ -1,3 +1,5 @@
+import fi from "@/client/util/locales/fi"
+
 export const formatKeyData = (data: any, programmeData: any) => {
   const { Kandiohjelmat, Maisteriohjelmat, metadata } = data
 
@@ -13,33 +15,14 @@ export const formatKeyData = (data: any, programmeData: any) => {
     const matchedProgramme = programmes.find(
       (programme: any) => programme.key === kandiohjelma['Koulutusohjelman koodi'].trim(),
     )
-    const redLights = [
-      'Vetovoimaisuus',
-      'Opintojen sujuvuus ja valmistuminen',
-      'Palaute ja työllistyminen',
-      'Resurssien käyttö',
-    ].filter(field => kandiohjelma[field] === 'Punainen')
-
-    const yellowLights = [
-      'Vetovoimaisuus',
-      'Opintojen sujuvuus ja valmistuminen',
-      'Palaute ja työllistyminen',
-      'Resurssien käyttö',
-    ].filter(field => kandiohjelma[field] === 'Keltainen')
 
     return {
       koulutusohjelmakoodi: kandiohjelma['Koulutusohjelman koodi'],
       koulutusohjelma: matchedProgramme && matchedProgramme.name,
       values: kandiohjelma,
-      vetovoimaisuus: kandiohjelma['Vetovoimaisuus'],
-      lapivirtaus: kandiohjelma['Opintojen sujuvuus ja valmistuminen'],
-      opiskelijapalaute: kandiohjelma['Palaute ja työllistyminen'],
-      resurssit: kandiohjelma['Resurssien käyttö'],
       year: kandiohjelma['Vuosi'],
       international: matchedProgramme?.international,
       level: matchedProgramme?.level,
-      redLights: redLights,
-      yellowLights: yellowLights,
     }
   })
 
@@ -47,39 +30,24 @@ export const formatKeyData = (data: any, programmeData: any) => {
     const matchedProgramme = programmes.find(
       (programme: any) => programme.key === maisteriohjelma['Koulutusohjelman koodi'].trim(),
     )
-    const redLights = [
-      'Vetovoimaisuus',
-      'Opintojen sujuvuus ja valmistuminen',
-      'Palaute ja työllistyminen',
-      'Resurssien käyttö',
-    ].filter(field => maisteriohjelma[field] === 'Punainen')
-
-    const yellowLights = [
-      'Vetovoimaisuus',
-      'Opintojen sujuvuus ja valmistuminen',
-      'Palaute ja työllistyminen',
-      'Resurssien käyttö',
-    ].filter(field => maisteriohjelma[field] === 'Keltainen')
 
     return {
       koulutusohjelmakoodi: maisteriohjelma['Koulutusohjelman koodi'],
       koulutusohjelma: matchedProgramme && matchedProgramme.name,
       values: maisteriohjelma,
-      vetovoimaisuus: maisteriohjelma['Vetovoimaisuus'],
-      lapivirtaus: maisteriohjelma['Opintojen sujuvuus ja valmistuminen'],
-      opiskelijapalaute: maisteriohjelma['Palaute ja työllistyminen'],
-      resurssit: maisteriohjelma['Resurssien käyttö'],
       year: maisteriohjelma['Vuosi'],
       international: matchedProgramme?.international,
       level: matchedProgramme?.level,
-      redLights: redLights,
-      yellowLights: yellowLights,
     }
   })
 
   const meta = metadata.map((m: any) => ({
-    arviointialue: m['Arviointialue'],
-    avainluvunArvo: m['Avainluvun nimi_fi'],
+    yksikko: m['Yksikkö'],
+    kynnysarvot: m['Kynnysarvot'],
+    ohjelmanTaso: m['Ohjelman taso'],
+    liikennevalo: m['Liikennevalo'],
+    mittarinRajat: m['Mittarin rajat'],
+    arviointialue: m['Arviointialue_fi'],
     avainluvunNimi: {
       fi: m[`Avainluvun nimi_fi`],
       se: m[`Avainluvun nimi_se`],
@@ -90,11 +58,7 @@ export const formatKeyData = (data: any, programmeData: any) => {
       se: m[`Määritelmä_se`],
       en: m[`Määritelmä_en`],
     },
-    ohjelmanTaso: m['Ohjelman taso'],
-    kynnysarvot: m['Kynnysarvot'],
-    yksikko: m['Yksikkö'],
-    liikennevalo: m['Liikennevalo'],
-    mittarinRajat: m['Mittarin rajat'],
+    avainluvunArvo: m['Avainluvun nimi_fi'],
   }))
 
   return { kandiohjelmat, maisteriohjelmat, metadata: meta }
