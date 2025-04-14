8e1656fdc2b3d2b364cad52614d11e64a36027d0
tfhuhtal <tuomas.huhtala@icloud.com>
2025-04-14 12:36:23 +0300

    fix

--- server/index.ts
+++ server/index.ts
@@ -39,10 +39,8 @@ const server = new http.Server(app)
 
 createWebsocketServer(server)
 
-initializeSentry(app)
+initializeSentry()
 
-app.use(Sentry.Handlers.requestHandler())
-app.use(Sentry.Handlers.tracingHandler())
 app.use(express.json({ limit: '50mb' }))
 app.use(compression())
 app.use(accessLogger)
@@ -57,7 +55,7 @@ if (AUTOMATIC_IAM_PERMISSIONS_ENABLED) {
 app.use(currentUserMiddleware)
 app.use('/api', routes)
 
-app.use(Sentry.Handlers.errorHandler())
+Sentry.setupExpressErrorHandler(app)
 app.use(errorMiddleware)
 
 if (inProduction || inStaging) {
