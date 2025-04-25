03df588e064678651b933710170d73689398664b
sutigit <panyanilben@gmail.com>
2025-04-25 14:34:47 +0300

    init

--- client/components/V1/ProgrammeHomeView/ProgrammeKeyDataTableComponent.tsx
+++ client/components/V1/ProgrammeHomeView/ProgrammeKeyDataTableComponent.tsx
@@ -1,5 +1,41 @@
+import { Table, TableRow, TableCell } from '../Generic/TableComponent'
+import { Typography, Tooltip } from '@mui/material'
+import { useTranslation } from 'react-i18next'
+
 const programmeKeyDataTableComponent = () => {
-  return <div>Keydata table</div>
+  const { t } = useTranslation()
+
+  return (
+    <Table>
+      <TableRow isHeader>
+        <TableCell></TableCell>
+        <TableCell>
+          <Typography variant="regularSmall">{t('keyData:vetovoimaisuus')}</Typography>
+        </TableCell>
+        <TableCell>
+          <Typography variant="regularSmall">{t('keyData:lapivirtaus')}</Typography>
+        </TableCell>
+        <TableCell>
+          <Typography variant="regularSmall">{t('keyData:opiskelijapalaute')}</Typography>
+        </TableCell>
+        <TableCell>
+          <Typography variant="regularSmall">{t('keyData:resurssit')}</Typography>
+        </TableCell>
+        <TableCell>
+          <Typography variant="regularSmall">{t('keyData:actions')}</Typography>
+        </TableCell>
+      </TableRow>
+
+      <TableRow>
+        <TableCell></TableCell>
+        <TableCell></TableCell>
+        <TableCell></TableCell>
+        <TableCell></TableCell>
+        <TableCell></TableCell>
+        <TableCell></TableCell>
+      </TableRow>
+    </Table>
+  )
 }
 
 export default programmeKeyDataTableComponent
