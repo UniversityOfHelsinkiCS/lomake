b186860bc2fd9288f7df72a6cb19848de91ef37a
tfhuhtal <tuomas.huhtala@icloud.com>
2025-04-29 15:56:39 +0300

    imported keydata to intervention procedure

--- client/components/V1/Generic/InterventionProcedure.tsx
+++ client/components/V1/Generic/InterventionProcedure.tsx
@@ -1,17 +1,20 @@
 import { useParams } from "react-router"
 import { useTranslation } from "react-i18next"
 import { Box, Typography } from "@mui/material"
+import { useFetchSingleKeyData } from "@/client/hooks/useFetchKeyData"
+import { useSelector } from "react-redux"
+import { RootState } from "@/client/util/store"
+import { SingleKeyData } from "@/shared/lib/types"
 
 const InterventionProcedure = () => {
-  const { t } = useTranslation()
-
-  const handleClick = () => {
-
-  }
+  const { programme: programmeKey } = useParams<{ programme: string }>()
+  const keyData: SingleKeyData = useFetchSingleKeyData(programmeKey)
+  const lang = useSelector((state: RootState) => state.language) as 'fi' | 'se' | 'en'
 
   return (
     <Box sx={{ alignContent: 'left', mt: '4rem' }}>
       <Typography variant="h1">INTERVENTION DOCUMENT</Typography>
+      <Typography>{keyData.programme.koulutusohjelma[lang]}</Typography>
     </Box>
   )
 }
