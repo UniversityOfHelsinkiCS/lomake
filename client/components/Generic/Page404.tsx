6d885800eab2baa8485aad86e37b879da250a872
sutigit <panyanilben@gmail.com>
2025-05-05 12:55:03 +0300

    update 404

--- client/components/Generic/Page404.tsx
+++ client/components/Generic/Page404.tsx
@@ -3,7 +3,7 @@ import { Typography, Box } from '@mui/material'
 const Page404 = () => {
   return (
     <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', paddingTop: '5rem' }}>
-      <Typography variant="h2">PAGE NOT FOUND</Typography>
+      <Typography variant="h1">PAGE NOT FOUND</Typography>
     </Box>
   )
 }
