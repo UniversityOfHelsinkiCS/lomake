0b2676ceaca6cacde3576e8fdc7c0f9091a0dbc6
tfhuhtal <tuomas.huhtala@icloud.com>
2025-08-12 13:23:23 +0300

    fix: more test tweaking

--- cypress/integration/IAMpermissions2.spec.js
+++ cypress/integration/IAMpermissions2.spec.js
@@ -10,7 +10,7 @@ describe('IAM permission tests', () => {
   it('Doctoral kosu who is also a regular kosu gets writing rights to all programmes', () => {
     cy.login('cypressDoctoralKosuAndRegularKosuUser')
     cy.visit('/yearly')
-    cy.get('[data-cy^=colortable-link-to]').should('have.have.length', 54)
+    cy.get('[data-cy^=colortable-link-to]').should('have.have.length', 55)
 
     cy.hasAccess('cypressDoctoralKosuAndRegularKosuUser', 'T920103', { read: true, write: true, admin: false })
     cy.hasAccess('cypressDoctoralKosuAndRegularKosuUser', 'KH50_004', { read: true, write: true, admin: false })
@@ -61,11 +61,11 @@ describe('IAM permission tests', () => {
   it('Jory group who has also writing rights to Faculty Evaluation works', () => {
     cy.login('cypressFacultyKatselmusUser')
     cy.visit('/yearly')
-    cy.get('[data-cy^=colortable-link-to]').should('have.have.length', 28)
+    cy.get('[data-cy^=colortable-link-to]').should('have.have.length', 29)
     cy.visit('/evaluation')
-    cy.get('[data-cy^=colortable-link-to]').should('have.have.length', 28)
+    cy.get('[data-cy^=colortable-link-to]').should('have.have.length', 29)
     cy.visit('/degree-reform')
-    cy.get('[data-cy^=colortable-link-to]').should('have.have.length', 28)
+    cy.get('[data-cy^=colortable-link-to]').should('have.have.length', 29)
     cy.visit('/evaluation-faculty')
     cy.get('[data-cy^=colortable-link-to]').should('have.have.length', 12)
 
