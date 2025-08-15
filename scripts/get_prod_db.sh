5b8a892afeb0cc1a368c73940917f896e13e30a1
tfhuhtal <tuomas.huhtala@icloud.com>
2025-08-15 14:01:35 +0300

    feat: get prod db script using s3 bucket

--- scripts/get_prod_db.sh
+++ scripts/get_prod_db.sh
@@ -4,21 +4,15 @@ CONTAINER=lomake_db
 SERVICE_NAME=db
 DB_NAME=postgres
 
-# current_date=$(date +"%Y%m%d")
-LOMAKE_FILE_NAME="lomake.sql.gz"
-
-SERVER=toska.cs.helsinki.fi
-SERVER_PATH=/home/toska_user/most_recent_backup_store/
-MELKKI=melkki.cs.helsinki.fi
-BACKUP_PATH=${SERVER_PATH}${LOMAKE_FILE_NAME}
+current_date=$(date +"%Y%m%d")
+FILE_NAME="lomake_${current_date}.sql.gz"
+FOLDER_NAME="lomake"
 
 PROJECT_ROOT=$(dirname $(dirname $(realpath "$0")))
 BACKUPS=$PROJECT_ROOT/backups/
 DOCKER_COMPOSE=$PROJECT_ROOT/docker-compose.yml
 
-USER_DATA_FILE_PATH=$PROJECT_ROOT/scripts/my_username
-
-username=""
+S3_CONF=~/.s3cfg
 
 retry () {
     for i in {1..60}
@@ -27,32 +21,20 @@ retry () {
     done
 }
 
-get_username() {
-  # Check if username has already been set
-  [ -z "$username" ]|| return 0
-
-  # Check if username is saved to data file and ask it if not
-  if [ ! -f "$USER_DATA_FILE_PATH" ]; then
-    echo ""
-    echo "!! No previous username data found. Will ask it now !!"
-    echo "Enter your Uni Helsinki username:"
-    read username
-    echo $username > $USER_DATA_FILE_PATH
-    echo "Succesfully saved username"
-    echo ""
-  fi
-
-  # Set username
-  username=$(cat $USER_DATA_FILE_PATH | head -n 1)
-}
+if [ ! -f "$S3_CONF" ]; then
+  echo ""
+  echo "!! No config file for s3 bucket !!"
+  echo "Create file for path ~/.s3cfg and copy the credetials from version.helsinki.fi"
+  echo ""
+  return 0
+fi
 
 echo "Creating backups folder"
 mkdir -p ${BACKUPS}
 
 echo "Fetching a new dump"
-get_username
 
-scp -r -o ProxyCommand="ssh -l $username -W %h:%p $MELKKI" $username@$SERVER:$BACKUP_PATH $BACKUPS
+s3cmd -c $S3_CONF get "s3://psyduck/${FOLDER_NAME}/${FILE_NAME}" $BACKUPS
 
 echo "Removing database and related volume"
 docker-compose -f $DOCKER_COMPOSE down -v
@@ -63,4 +45,4 @@ docker-compose -f $DOCKER_COMPOSE up -d $SERVICE_NAME
 retry docker-compose -f $DOCKER_COMPOSE exec $SERVICE_NAME pg_isready --dbname=$DB_NAME
 
 echo "Populating Lomake"
-docker exec -i $CONTAINER /bin/bash -c "gunzip | psql -U postgres" < ${BACKUPS}${LOMAKE_FILE_NAME}
+docker exec -i $CONTAINER /bin/bash -c "gunzip | psql -U postgres" < ${BACKUPS}${FILE_NAME}
