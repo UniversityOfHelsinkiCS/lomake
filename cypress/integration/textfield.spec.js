30b1e0314596f8f2b1c8f8f26e5c381a847b57ab
Matti Luukkainen <mluukkai@iki.fi>
2026-01-23 14:56:08 +0200

    update year to 2026

--- cypress/integration/textfield.spec.js
+++ cypress/integration/textfield.spec.js
@@ -2,7 +2,7 @@
 import '../support/commands'
 import { possibleUsers } from '../../config/mockHeaders'
 
-const year = 2025
+const year = 2026
 
 describe('Textfield tests', () => {
   beforeEach(() => {
@@ -24,6 +24,7 @@ describe('Textfield tests', () => {
 
     cy.get('[data-cy=updateDeadline]').click()
     cy.get('[data-cy=form-10-deadline]').contains('14.')
+
     cy.visit(`/v1/programmes/10/KH50_005/${year}`)
   })
 
