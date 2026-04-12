6f6f9b8d5085a022a8f22504a198bc25cf46dd40
outisa <savolainen.outi@gmail.com>
2026-04-12 23:51:55 +0300

    Remove comma added by accident

--- client/components/V1/ProgrammeHomeView/InterventionComponent.tsx
+++ client/components/V1/ProgrammeHomeView/InterventionComponent.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-alert */
 /* eslint-disable @typescript-eslint/no-floating-promises */
-import { useEffect, useState, } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router'
 import { useTranslation } from 'react-i18next'
 import { useSelector } from 'react-redux'
