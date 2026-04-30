49ade3408ca6617240b33209af2b2598e5e51205
kaabero <katri.laamala@outlook.com>
2026-04-30 11:48:08 +0300

    remove kielikeskus from faculties

--- server/controllers/facultyController.js
+++ server/controllers/facultyController.js
@@ -1,9 +1,11 @@
 import Faculty from '../models/faculty.js'
 import logger from '../util/logger.js'
+import { Op } from 'sequelize'
 
 const getAll = async (_, res) => {
   try {
     const data = await Faculty.findAll({
+      where: { code: { [Op.ne]: 'H906' } },
       include: ['ownedProgrammes', 'companionStudyprogrammes'],
       order: [['code', 'ASC']],
     })
