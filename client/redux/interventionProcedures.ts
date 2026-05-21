2260b10c1b911b297d85030d0d79ab0b3d9da0ac
kaabero <katri.laamala@outlook.com>
2026-05-21 13:30:25 +0300

    add create and update mutations for intervention procedures

--- client/redux/interventionProcedures.ts
+++ client/redux/interventionProcedures.ts
@@ -5,6 +5,11 @@ interface GetInterventionProceduresArgs {
   studyprogrammeKey: string
 }
 
+interface CreateInterventionProcedureArgs {
+  studyprogrammeKey: string
+  year: number
+}
+
 const interventionProceduresApi = RTKApi.injectEndpoints({
   endpoints: builder => ({
     getProgrammesInterventionProcedures: builder.query<InterventionProcedureType[], GetInterventionProceduresArgs>({
@@ -12,7 +17,7 @@ const interventionProceduresApi = RTKApi.injectEndpoints({
         url: `/interventionprocedures/${studyprogrammeKey}`,
         method: 'get',
       }),
-      providesTags: ['InterventionProcedures'],
+      providesTags: [{ type: 'InterventionProcedures' }],
     }),
     getInterventionProcedures: builder.query<InterventionProcedureType[], void>({
       query: () => ({
@@ -20,8 +25,28 @@ const interventionProceduresApi = RTKApi.injectEndpoints({
         method: 'get',
       }),
     }),
+    createInterventionProcedure: builder.mutation<InterventionProcedureType, CreateInterventionProcedureArgs>({
+      query: ({ studyprogrammeKey, year }) => ({
+        url: `/interventionprocedures/`,
+        method: 'post',
+        body: { studyprogrammeKey, year },
+      }),
+      invalidatesTags: [{ type: 'InterventionProcedures' }],
+    }),
+    updateInterventionProcedure: builder.mutation<InterventionProcedureType, { id: number; startYear: number }>({
+      query: ({ id, startYear }) => ({
+        url: `/interventionprocedures/${id}`,
+        method: 'put',
+        body: { startYear },
+      }),
+      invalidatesTags: [{ type: 'InterventionProcedures' }],
+    }),
   }),
 })
 
-export const { useGetProgrammesInterventionProceduresQuery, useGetInterventionProceduresQuery } =
-  interventionProceduresApi
+export const {
+  useGetProgrammesInterventionProceduresQuery,
+  useGetInterventionProceduresQuery,
+  useCreateInterventionProcedureMutation,
+  useUpdateInterventionProcedureMutation,
+} = interventionProceduresApi
